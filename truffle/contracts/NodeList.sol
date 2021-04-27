01b28ce0d846c397401d079ee36eb054b5f4f43b
bogdan sivochkin <syi3@ya.ru>
2021-04-27 11:58:54 +0300

    unique Node in NodeList.sol by p2pAddress

--- truffle/contracts/NodeList.sol
+++ truffle/contracts/NodeList.sol
@@ -7,40 +7,44 @@ contract NodeList {
   	bool    enable;
     address nodeWallet;
     bytes   p2pAddress;
-    bytes32 pubKey;
-    
+    bytes pubKey;
+
   }
 
 
-   mapping (address => Node) public listNode;
-   address[] private keys;
+   mapping (bytes => Node) public listNode;
+   bytes[] private p2pAddrs;
 
-  function addNode(address _nodeWallet, bytes memory _p2pAddress, bytes32 _pubKey, bool _enable) external /*onlyOwner*/ {
-      Node storage node = listNode[_nodeWallet];
+  function addNode(address _nodeWallet, bytes memory _p2pAddress, bytes calldata _pubKey, bool _enable) external isNewNode(_p2pAddress) /*onlyOwner*/ {
+      require(_nodeWallet != address(0), "0 address");
+      Node storage node = listNode[_p2pAddress];
       node.nodeWallet   = _nodeWallet;
       node.p2pAddress   = _p2pAddress;
       node.pubKey       = _pubKey;
       node.enable       = _enable;
-      keys.push(_nodeWallet);
+      p2pAddrs.push(_p2pAddress);
   }
 
-  function getNode(address _nodeWallet) external view returns (Node memory) {
-  	return listNode[_nodeWallet];
+  function getNode(bytes memory _nodeAddr) external view returns (Node memory) {
+  	return listNode[_nodeAddr];
   }
 
-  function getAllNodeWallet() external view returns (address[] memory) {
-  	return keys;
+  function getAllNodesAddrs() external view returns (bytes[] memory) {
+  	return p2pAddrs;
   }
 
   function getNodes() external view returns (Node[] memory){
-      Node[] memory nodes = new Node[](keys.length);
-      for (uint i = 0; i < keys.length; i++) {
-          Node storage node = listNode[keys[i]];
+      Node[] memory nodes = new Node[](p2pAddrs.length);
+      for (uint i = 0; i < p2pAddrs.length; i++) {
+          Node storage node = listNode[p2pAddrs[i]];
           nodes[i] = node;
       }
       return nodes;
   }
 
-
+    modifier isNewNode(bytes memory _nodeAddr) {
+        require(listNode[_nodeAddr].nodeWallet == address(0),"node allready exists");
+        _;
+    }
 
 }
