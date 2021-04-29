6adac79c9f227e54dcb3f142e9ddfabd2ad1a598
bogdan sivochkin <syi3@ya.ru>
2021-04-29 16:15:39 +0300

    NodeList.sol update

--- truffle/contracts/NodeList.sol
+++ truffle/contracts/NodeList.sol
@@ -7,29 +7,38 @@ contract NodeList {
   	bool    enable;
     address nodeWallet;
     bytes   p2pAddress;
-    bytes pubKey;
-
+    address blsPointAddr;
+    bytes   blsPubKey;
+    uint64 nodeId;
   }
 
+   mapping (address => Node) public listNode;
+   address[] public p2pAddrs;
+    Node[] public nodes;
 
-   mapping (bytes => Node) public listNode;
-   bytes[] private p2pAddrs;
-
-  function addNode(address _nodeWallet, bytes memory _p2pAddress, bytes calldata _pubKey, bool _enable) external isNewNode(_p2pAddress) /*onlyOwner*/ {
+  function addNode(address _nodeWallet, bytes memory _p2pAddress, address _blsPointAddr, bytes memory _blsPubKey, bool _enable) external isNewNode(_blsPointAddr) /*onlyOwner*/ {
       require(_nodeWallet != address(0), "0 address");
-      Node storage node = listNode[_p2pAddress];
+      require(_blsPointAddr != address(0), "0 address");
+      Node storage node = listNode[_blsPointAddr];
+      node.nodeId  = getNewNodeId();
       node.nodeWallet   = _nodeWallet;
       node.p2pAddress   = _p2pAddress;
-      node.pubKey       = _pubKey;
+      node.blsPointAddr = _blsPointAddr;
+      node.blsPubKey    = _blsPubKey;
       node.enable       = _enable;
-      p2pAddrs.push(_p2pAddress);
+      p2pAddrs.push(_blsPointAddr);
+      nodes.push(node);
   }
 
-  function getNode(bytes memory _nodeAddr) external view returns (Node memory) {
-  	return listNode[_nodeAddr];
+    function getNewNodeId() internal returns (uint64){
+        return uint64(nodes.length);
+    }
+
+  function getNode(address _blsPubAddr) external view returns (Node memory)  {
+  	return listNode[_blsPubAddr];
   }
 
-  function getAllNodesAddrs() external view returns (bytes[] memory) {
+  function getAllNodesAddrs() external view returns (address[] memory) {
   	return p2pAddrs;
   }
 
@@ -42,9 +51,51 @@ contract NodeList {
       return nodes;
   }
 
-    modifier isNewNode(bytes memory _nodeAddr) {
-        require(listNode[_nodeAddr].nodeWallet == address(0),"node allready exists");
+    function getBLSPubKeys() external view returns (bytes[] memory){
+        bytes[] memory pubKeys = new bytes[](p2pAddrs.length);
+        for (uint i = 0; i < p2pAddrs.length; i++) {
+            Node storage node = listNode[p2pAddrs[i]];
+            pubKeys[i] = node.blsPubKey;
+        }
+        return pubKeys;
+    }
+
+    function getNodeBLSAddrs() external view returns (address[] memory){
+        address[] memory blsAddrs= new address[](p2pAddrs.length);
+        for (uint i = 0; i < p2pAddrs.length; i++) {
+            Node storage node = listNode[p2pAddrs[i]];
+            blsAddrs[i] = node.blsPointAddr;
+        }
+        return blsAddrs;
+    }
+
+    modifier isNewNode(address _nodeBLSAddr) {
+        require(listNode[_nodeBLSAddr].blsPointAddr == address(0),string(abi.encodePacked("node ", toString(_nodeBLSAddr), " allready exists")));
         _;
     }
 
+    modifier existingNode(address addr) {
+        require(listNode[addr].blsPointAddr != address(0), string(abi.encodePacked("node ", toString(addr), " does not exist")));
+        _;
+    }
+
+
+
+    /// @notice Преобразовать адрес в строку для require()
+    function toString(address account) public pure returns (string memory s) {
+        bytes memory alphabet = "0123456789abcdef";
+        bytes memory data = abi.encodePacked(account);
+        bytes memory result = new bytes(2 + data.length * 2);
+        result[0] = '0';
+        result[1] = 'x';
+        for (uint i = 0; i < data.length; i++) {
+            result[2+i*2] = alphabet[uint(uint8(data[i] >> 4))];
+            result[3+i*2] = alphabet[uint(uint8(data[i] & 0x0f))];
+        }
+        return string(result);
+    }
+
+    function nodeExists(address _blsPubAddr) public view returns (bool) {
+        return listNode[_blsPubAddr].blsPointAddr != address(0);
+    }
 }
