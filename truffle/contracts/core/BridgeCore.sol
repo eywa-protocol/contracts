7525a33d965a737536426a027796cb174d7b4bec
bogdan sivochkin <syi3@ya.ru>
2021-06-10 13:57:44 +0300

    opposite externalBridgesByChainId  mapping in bridgeCore.sol

--- truffle/contracts/core/BridgeCore.sol
+++ truffle/contracts/core/BridgeCore.sol
@@ -6,14 +6,25 @@ contract BridgeCore {
 //TODO: initializer
   function initialize(address listNode) public /*initializer*/ {
       _listNode = listNode;
-      
   }
 
   address public    _listNode;
   address private   oppositeBridge;
   uint256 private   requestCount = 1;
   mapping(address => bool) public whiteList;
-  
+  mapping(address => uint) public bridgeList;
+  mapping(uint => address) public externalBridgesByChainId;
+
+
+  function addBridge(address bridge, uint chainId) public /*onlyOwner, bridgeDoesNotExist*/ {
+    externalBridgesByChainId[chainId] = bridge;
+  }
+
+
+
+  function getBridgeByChainId(uint chainId) public /*onlyOwner*/ returns (address) {
+    return externalBridgesByChainId[chainId];
+  }
 
 
   function prepareRqId(bytes memory  _selector, address receiveSide) internal returns (bytes32, address){
@@ -37,7 +48,8 @@ contract BridgeCore {
     bytes32 requestId,
     bytes   selector,
     address receiveSide,
-    address oppositeBridge
+    address oppositeBridge,
+    uint chainid
   );
 
   event ReceiveRequest(string reqId, address receiveSide, bytes32 tx);
