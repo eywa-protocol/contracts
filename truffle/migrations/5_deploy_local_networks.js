253c76577a0764a6a5c5d3fa372be14f9e66c4dc
v0ld <v0ld@v0ld.com>
2021-06-13 18:59:12 +0700

    fix redundant action in case truffle test e2e

--- truffle/migrations/5_deploy_local_networks.js
+++ truffle/migrations/5_deploy_local_networks.js
@@ -1,5 +1,7 @@
 const { migrateLocalnetwork} = require('./migrate');
 
 module.exports = async (deployer, network, accounts) => {
+	if (network === 'network1' || network === 'network2' || network === 'network3') {
       await migrateLocalnetwork(deployer, network, accounts);
+  	}
 }
