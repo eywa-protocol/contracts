32a62c87916f8d38f0ec114f000d6e21f618aa28
Dmitriy Gogolev <gogolevdms@gmail.com>
2021-08-05 09:30:39 +0300

    update style in config

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomiclabs/hardhat-truffle5");
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 require("hardhat-gas-reporter");
+
 const env = require('dotenv').config({ path: '../../.env' })
 const networkConfig = require('./helper-hardhat-config.json');
 
@@ -21,70 +22,78 @@ async function getKey(network) {
 
 
 module.exports = {
-    defaultNetwork: "hardhat",
-    networks: {
-        hardhat: {
-            // // If you want to do some forking, uncomment this
-            // forking: {
-            //   url: MAINNET_RPC_URL
-            // }
-        },
-        localhost: {
-        },
-        rinkeby: {
-            url:  networkConfig.rinkeby.rpcUrl.replace('ws','http').replace('ws/',''),
-            accounts: [RINKEBY_PRIVATE_KEY]
-        },
-        bsctestnet: {
-            url:  'https://data-seed-prebsc-1-s1.binance.org:8545/',
-            accounts: [BSC_PRIVATE_KEY]
-        },
-        mumbai:{
-            url:  networkConfig.mumbai.rpcUrl.replace('ws','http').replace('ws/',''),
-            accounts: [MUMBAI_PRIVATE_KEY]
-        },
-        network1: {
-             url: networkConfig.network1.rpcUrl.replace('ws','http'),
-             accounts:[env.parsed.PRIVATE_KEY_NETWORK1]
-
-        },
-        network2: {
-	     url: networkConfig.network2.rpcUrl.replace('ws','http'),
-             accounts:[env.parsed.PRIVATE_KEY_NETWORK2]
-        },
-        network3: {
-             url: networkConfig.network3.rpcUrl.replace('ws','http'),
-             accounts:[env.parsed.PRIVATE_KEY_NETWORK3]
-        },
-        ganache: {
-            url:  networkConfig.ganache.rpcUrl,
-            accounts: [GANACHE_PRIVATE_KEY]
-        },
-        hecotestnet:{
-            url: networkConfig.hecotestnet.rpcUrl.split('ws').join('http'),
-            accounts: [HECO_PRIVATE_KEY]
-        }
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      // // If you want to do some forking, uncomment this
+      // forking: {
+      //   url: MAINNET_RPC_URL
+      // }
     },
-    etherscan: {
-      apiKey: getKey(process.argv[5] || process.env.HARDHAT_NETWORK)
+    localhost: {
+        //
     },
-    gasReporter: {
-      currency: "USD",
-      gasPrice: 100,
-      // enabled: process.env.REPORT_GAS ? true : false,
+    rinkeby: {
+      url: networkConfig.rinkeby.rpcUrl.replace('ws','http').replace('ws/',''),
+      accounts: [RINKEBY_PRIVATE_KEY]
     },
-    solidity: {
-        compilers: [{
-                version: "0.8.0",
-                settings: {
-                  optimizer: {
-                    enabled: true,
-                    runs: 200
-                  }
-                }
-            }]
+    bsctestnet: {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+      accounts: [BSC_PRIVATE_KEY]
     },
-    mocha: {
-        timeout: 100000
+    mumbai:{
+        url: networkConfig.mumbai.rpcUrl.replace('ws','http').replace('ws/',''),
+        accounts: [MUMBAI_PRIVATE_KEY]
+    },
+    network1: {
+       url: networkConfig.network1.rpcUrl.replace('ws','http'),
+       accounts: [env.parsed.PRIVATE_KEY_NETWORK1]
+    },
+    network2: {
+      url: networkConfig.network2.rpcUrl.replace('ws','http'),
+      accounts: [env.parsed.PRIVATE_KEY_NETWORK2]
+    },
+    network3: {
+      url: networkConfig.network3.rpcUrl.replace('ws','http'),
+      accounts: [env.parsed.PRIVATE_KEY_NETWORK3]
+    },
+    ganache: {
+      url: networkConfig.ganache.rpcUrl,
+      accounts: [GANACHE_PRIVATE_KEY]
+    },
+    hecotestnet:{
+      url: networkConfig.hecotestnet.rpcUrl.split('ws').join('http'),
+      accounts: [HECO_PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: getKey(process.argv[5] || process.env.HARDHAT_NETWORK)
+  },
+  gasReporter: {
+    currency: "USD",
+    gasPrice: 100,
+    // enabled: process.env.REPORT_GAS ? true : false,
+  },
+  solidity: {
+    compilers: [{
+      version: "0.8.0",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }, {
+      version: "0.7.6",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }]
+  },
+  mocha: {
+    timeout: 100000
+  }
 };
