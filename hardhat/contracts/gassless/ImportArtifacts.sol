b1670ee549687f933ee834d5bb95ec8a3dfe415e
Dmitriy Gogolev <gogolevdms@gmail.com>
2021-08-05 12:24:17 +0300

    fix compiler version

--- hardhat/contracts/gassless/ImportArtifacts.sol
+++ hardhat/contracts/gassless/ImportArtifacts.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.7.6;
 pragma abicoder v2;
 
 //"import" it into our project for Truffle to generate artifacts
-//import "@opengsn/paymasters/contracts/WhitelistPaymaster.sol";
+// import "@opengsn/paymasters/contracts/WhitelistPaymaster.sol";
 
 import "@opengsn/contracts/src/RelayHub.sol";
