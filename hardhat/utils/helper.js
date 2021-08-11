16963d35de5e9e9e9c452d15788ef4cf9596345d
Dmitriy Gogolev <gogolevdms@gmail.com>
2021-08-11 11:21:00 +0300

    fix config path in helper.js

--- hardhat/utils/helper.js
+++ hardhat/utils/helper.js
@@ -1,13 +1,10 @@
 "use strict";
 const Web3 = require('web3');
 const web3 = new Web3();
-const { networks }         = require('../packages/bridge/hardhat.config');
+const { networks } = require('../hardhat.config');
 
 function toWei(n) { return web3.utils.toWei(n, 'ether');}
 function fromWei(n) { return web3.utils.fromWei(n, 'ether');}
-
-
-
 
 const checkoutProvider = (argv) => {
 
@@ -17,7 +14,7 @@ const checkoutProvider = (argv) => {
     const web3Net2 = new Web3.providers.WebsocketProvider(networks[argv.net2].url.replace('http','ws'));
     const web3Net3 = new Web3.providers.WebsocketProvider(networks[argv.net3].url.replace('http','ws'));
 
-    return {web3Net1, web3Net2, web3Net3};
+    return { web3Net1, web3Net2, web3Net3 };
   }
 
   if(argv.typenet === 'teststand'){
@@ -26,18 +23,17 @@ const checkoutProvider = (argv) => {
     const web3Net2 = networks[argv.net2].provider();
     const web3Net3 = networks[argv.net3].provider();
 
-    return {web3Net1, web3Net2, web3Net3};
+    return { web3Net1, web3Net2, web3Net3 };
   }
-}
+};
 
 const timeout = async (ms) => {
-        return new Promise(resolve => setTimeout(resolve, ms));
-}
-
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
 
 module.exports = {
-    toWei,
-    fromWei,
-    checkoutProvider,
-    timeout
+  toWei,
+  fromWei,
+  checkoutProvider,
+  timeout
 };
