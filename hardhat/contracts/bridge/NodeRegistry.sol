96fc668c34aa8e2d3c0956aa61ad3e33174c9e9b
Vadim Gukasov <wadguk@yandex.ru>
2021-08-17 18:21:39 +0300

    Node registration added

---
+++ hardhat/contracts/bridge/NodeRegistry.sol
@@ -0,0 +1,144 @@
+pragma solidity 0.8.4;
+
+import '@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol';
+import '@opengsn/contracts/src/BaseRelayRecipient.sol';
+import './RelayerPool.sol';
+
+//  Создать новую ноду-релеер, требует наличия определенного залога COLLATERAL и
+//  автоматически создаёт новый смартконтракт Relayer pool, привязанный к этой ноде,
+//  COLLATERAL забирается с кошелька и помещается в связанный контракт Relayer pool.
+//  Статус в этом случае выставляется в значение online.
+//  Для успешного вызова этого метода обязательно требуется наличие COLLATERAL на кошельке,
+//  а также ему нужно передать Relayer key и другие параметры из пункта 4 из процесса регистрации
+
+contract NodeRegistry is BaseRelayRecipient {
+
+    using SafeERC20 for IERC20;
+
+    struct Node {
+        address owner;
+        address nodeWallet;
+        address nodeIdAddress;
+        address pool;
+        string  blsPubKey;
+        uint64  nodeId;
+        uint256 version;
+        uint256 relayerFeeNumerator; 
+        uint256 emissionRate;
+        RelayerPool.RelayerStatus status;
+        RelayerType nodeType;
+    }
+    
+    enum RelayerType { Validator, Fisher }
+        
+    address EYWA;
+    address consensus;
+    uint256 constant MIN_COLLATERAL = 1 ether;
+
+        
+    mapping (address => Node) public listNode;
+    mapping (address => mapping(address => bool)) public trustListForDex;
+    Node[] public nodes;
+
+    event AddedNode(address nodeIdAddress);
+    
+    constructor(address _EYWA, address _consensus){
+        EYWA = _EYWA;
+        consensus = _consensus;
+    }
+    
+    modifier isNewNode(address _nodeIdAddr) {
+        require(listNode[_nodeIdAddr].nodeWallet == address(0),string(abi.encodePacked("node ", convertToString(_nodeIdAddr), " allready exists")));
+        _;
+    }
+
+    modifier existingNode(address _nodeIdAddr) {
+        require(listNode[_nodeIdAddr].nodeWallet != address(0), string(abi.encodePacked("node ", convertToString(_nodeIdAddr), " does not exist")));
+        _;
+    }
+    
+    modifier onlyConsensus() {
+        require(_msgSender() == consensus, "only consensus");
+        _;
+    }
+
+    //TODO: discuss about check: listNode[_blsPointAddr] == address(0)
+    function addNode(Node memory _node) internal isNewNode(_node.nodeIdAddress) /*onlyOwner*/ {
+      require(_node.nodeWallet != address(0), "0 address");
+      require(_node.nodeIdAddress != address(0), "0 address");
+      _node.nodeId = getNewNodeId();
+      listNode[_node.nodeIdAddress] = _node;
+      nodes.push(_node);
+      //TODO: discuss about pemission for certain bridge
+      trustListForDex[_node.nodeWallet][address(0)] = true;
+      
+      emit AddedNode(_node.nodeIdAddress);
+    }
+
+    function getNewNodeId() internal returns (uint64){
+        return uint64(nodes.length);
+    }
+
+    function getNode(address _nodeIdAddress) external view returns (Node memory)  {
+  	   return listNode[_nodeIdAddress];
+    }
+
+    function getNodes() external view returns (Node[] memory){
+      return nodes;
+    }
+
+    function getBLSPubKeys() external view returns (string[] memory){
+        string[] memory pubKeys = new string[](nodes.length);
+        for (uint i = 0; i < nodes.length; i++) {
+            Node storage node = listNode[nodes[i].nodeIdAddress];
+            pubKeys[i] = node.blsPubKey;
+        }
+        return pubKeys;
+    }
+    
+    /// @notice Преобразовать адрес в строку для require()
+    function convertToString(address account) public pure returns (string memory s) {
+        bytes memory alphabet = "0123456789abcdef";
+        bytes memory data = abi.encodePacked(account);
+        bytes memory result = new bytes(2 + data.length * 2);
+        result[0] = '0';
+        result[1] = 'x';
+        for (uint i = 0; i < data.length; i++) {
+            result[2+i*2] = alphabet[uint(uint8(data[i] >> 4))];
+            result[3+i*2] = alphabet[uint(uint8(data[i] & 0x0f))];
+        }
+        return string(result);
+    }
+
+    function nodeExists(address _nodeIdAddr) public view returns (bool) {
+        return listNode[_nodeIdAddr].nodeWallet != address(0);
+    }
+
+    function checkPermissionTrustList(address node) external view returns (bool)  {
+        return trustListForDex[node][address(0)];
+    }
+    
+    
+    function setRelayerFee(uint256 _fee, address _nodeIdAddress ) external {
+        require(_msgSender() == listNode[_nodeIdAddress].owner, "only node owner");
+        RelayerPool(listNode[_nodeIdAddress].pool).setRelayerFeeNumerator(_fee);
+        //emit RelayerFeeSet(value);
+    }
+    
+    function setRelayerStatus(RelayerPool.RelayerStatus _status, address _nodeIdAddress) external onlyConsensus {
+        require(listNode[_nodeIdAddress].status != _status, Errors.SAME_VALUE);
+        listNode[_nodeIdAddress].status = _status;
+        RelayerPool(listNode[_nodeIdAddress].pool).setRelayerStatus(_status);
+        // emit RelayerStatusSet(_status);
+    }
+    
+    function createRelayer(Node memory _node, uint256 _deadline, uint8 _v, bytes32 _r, bytes32 _s) external {
+        RelayerPool relayerPool = new RelayerPool(_node.owner, address(this), _node.relayerFeeNumerator, _node.emissionRate, address(EYWA));
+        IERC20Permit(EYWA).permit(_msgSender(), address(this), MIN_COLLATERAL, _deadline, _v, _r, _s);
+        IERC20(EYWA).safeTransferFrom(_msgSender(), address(relayerPool), MIN_COLLATERAL);
+        addNode(_node);
+        this.setRelayerStatus(RelayerPool.RelayerStatus.Online, _node.nodeIdAddress);
+    }
+    
+    string public override versionRecipient = "2.2.3";
+}
---
+++ hardhat/contracts/bridge/RelayerPool.sol
@@ -0,0 +1,246 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.4;
+
+// https://confluence.digiu.ai/pages/viewpage.action?pageId=19202711
+
+import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
+import {EnumerableSet} from '@openzeppelin/contracts/utils/structs/EnumerableSet.sol';
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+import {ReentrancyGuard} from '@openzeppelin/contracts/security/ReentrancyGuard.sol';
+
+//todo discuss заморозка через оптимизацию array or enumerableSet with depositId
+//todo если решение через depositId - OK то добавляем view методы
+
+library Errors {
+    string public constant FEE_IS_TOO_LOW = 'FEE_IS_TOO_LOW';
+    string public constant FEE_IS_TOO_HIGH = 'FEE_IS_TOO_HIGH';
+    string public constant SAME_VALUE = 'SAME_VALUE';
+    string public constant ZERO_ADDRESS = 'ZERO_ADDRESS';
+    string public constant NOT_DEPOSIT_OWNER = 'NOT_DEPOSIT_OWNER';
+    string public constant DEPOSIT_IS_LOCKED = 'DEPOSIT_IS_LOCKED';
+    string public constant INSUFFICIENT_DEPOSIT = 'INSUFFICIENT_DEPOSIT';
+    string public constant DATA_INCONSISTENCY = 'DATA_INCONSISTENCY';  //todo discuss do we need it
+}
+
+// stores tokens
+contract Vault is Ownable {
+    using SafeERC20 for IERC20;
+}
+
+contract RelayerPool is Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+    using EnumerableSet for EnumerableSet.UintSet;
+    address internal payableToken;
+    
+    uint256 version;
+    enum RelayerStatus { Online, Offline, Inactive, BlackListed }
+    RelayerStatus internal relayerStatus;
+    
+    uint256 constant MIN_RELAYER_STAKING_TIME = 4 weeks;
+    uint256 constant MIN_STAKING_TIME = 2 weeks;
+
+    uint256 internal nextDepositId;
+    mapping (uint256 => Deposit) public deposits;  // id -> deposit
+    mapping (address => EnumerableSet.UintSet) internal userDepositIds;
+    mapping (address => uint256) userTotalDeposit;
+    uint256 internal totalDeposit;
+    uint256 internal lastShareRewardTimestamp;
+    mapping (address => uint256) userClaimed;
+    uint256 internal rewardPerTokenNumerator;
+    uint256 constant REWARD_PER_TOKEN_DENOMINATOR = 10*18;
+    uint256 internal minOwnerCollateral;
+
+    uint256 constant RELAYER_FEE_MIN_NUMERATOR = 200;
+    uint256 constant RELAYER_FEE_DENOMINATOR = 10000;
+    uint256  relayerFeeNumerator;
+    uint256  emissionRate;
+    address  poolOwner;
+    address  registry;
+    
+    Vault internal vault;
+    
+    
+    constructor(
+        //lastShareRewardTimestamp = block.timestamp;  
+        address _poolOwner,
+        address _registry,
+        uint256 _relayerFeeNumerator,
+        uint256 _emissionRate,
+        address _rewardToken
+    ) {
+        require(relayerFeeNumerator >= RELAYER_FEE_MIN_NUMERATOR, Errors.FEE_IS_TOO_LOW);
+        require(relayerFeeNumerator <= RELAYER_FEE_DENOMINATOR, Errors.FEE_IS_TOO_HIGH);
+        relayerFeeNumerator = _relayerFeeNumerator;
+        // todo discuss limits
+        emissionRate = _emissionRate;
+        require(_rewardToken != address(0), Errors.ZERO_ADDRESS);
+        payableToken = _rewardToken;
+
+    }
+    
+    modifier onlyRegistry() {
+        require(msg.sender == registry, "only registry");
+        _;
+    }
+
+    function getTotalDeposit() external view returns(uint256){
+        return totalDeposit;
+    }
+
+    struct Deposit {
+        address user;  // todo optimization it's possible to exclude
+        uint256 lockTill;  //todo optimization is possible uint40
+        uint256 amount;
+    }
+
+    event DepositPut (
+        address indexed user,
+        uint40 lockFor,
+        uint256 indexed id,
+        uint256 amount
+    );
+
+    event DepositWithdrawn (
+        address indexed user,
+        uint256 indexed id,
+        uint256 amount,
+        uint256 rest
+    );
+
+    /// @dev метод для вывода средств из пула, позволяет выводить вызывающему только те средства,
+    ///   которые были размещены с его адреса, учитывает сроки заморозки токенов при депозите,
+    ///   для владельца адреса Relayer owner address - min_relayer_staking_time=4 недели,
+    ///   для стейкеров пула Relayer pool - две недели, максимальное время стейкинга неограничено.
+    function withdraw(uint256 _depositId, uint256 _amount) external {
+        Deposit storage deposit = deposits[_depositId];
+        require(deposit.user == msg.sender, Errors.NOT_DEPOSIT_OWNER);
+        require(_amount <= deposit.amount, Errors.INSUFFICIENT_DEPOSIT);
+        require(block.timestamp >= deposit.lockTill, Errors.DEPOSIT_IS_LOCKED);
+        require(userDepositIds[msg.sender].contains(_depositId), Errors.DATA_INCONSISTENCY);
+
+        harvest();  // ensure user collected reward
+        userClaimed[msg.sender] -= _amount * rewardPerTokenNumerator / REWARD_PER_TOKEN_DENOMINATOR;
+
+        if (_amount < deposit.amount) {
+            deposit.amount -= _amount;
+            emit DepositWithdrawn(msg.sender, _depositId, _amount, deposit.amount);
+        } else {  // deposit.amount == amount, because of require condition above (take care!)
+            delete deposits[_depositId];  // free up storage slot
+            require(userDepositIds[msg.sender].remove(_depositId), Errors.DATA_INCONSISTENCY);
+            emit DepositWithdrawn(msg.sender, _depositId, _amount, 0);
+        }
+        userTotalDeposit[msg.sender] -= _amount;  // todo total deposit event
+        totalDeposit -= _amount;
+        if (msg.sender == owner()) {
+            require(totalDeposit <= userTotalDeposit[owner()] * 6, "small owner stake (ownerStaker*6 >= totalStake)");  //todo err msg
+            require(userTotalDeposit[owner()] >= minOwnerCollateral, "small owner stake (ownerStake >= _min_owner_collateral)");
+        }
+        IERC20(payableToken).safeTransfer(msg.sender, _amount);
+    }
+
+    /// @dev метод для отправки транзакциии в стейкинг пул, при добавлении проверяем условие,
+    ///   что максимальный суммарный стейк, Pool Stake не превышает Owner stake*6, а минимальнй стейк владельца
+    ///   при этом должен быть не менее COLLATERAL.
+    function deposit(uint256 _amount) external {
+        uint256 depositId = nextDepositId++;
+        uint256 lockTill;
+        if (msg.sender == owner()) {
+            lockTill = block.timestamp + MIN_RELAYER_STAKING_TIME;
+        } else {
+            lockTill = block.timestamp + MIN_STAKING_TIME;
+        }
+        deposits[depositId] = Deposit({
+            user: msg.sender,
+            lockTill: lockTill,
+            amount: _amount
+        });
+        userTotalDeposit[msg.sender] += _amount;
+        totalDeposit += _amount;
+        IERC20(payableToken).safeTransferFrom(msg.sender, address(this), _amount);
+        if (msg.sender != owner()) {
+            require(totalDeposit <= userTotalDeposit[owner()]*6, "small owner stake (ownerStaker*6 >= totalStake)");
+        }
+        userClaimed[msg.sender] += _amount * rewardPerTokenNumerator / REWARD_PER_TOKEN_DENOMINATOR;
+    }
+
+    /// @dev метод для сбора вознаграждений из смартконтракте Reward, доступен с адреса, который разместил средства,
+    ///  на замороженные средства также действует период заморозки
+    function harvest() public {
+        uint256 reward = (rewardPerTokenNumerator * userTotalDeposit[msg.sender] / REWARD_PER_TOKEN_DENOMINATOR
+            - userClaimed[msg.sender]);
+        if (reward == 0) {
+            return;
+        }
+        IERC20(payableToken).safeTransfer(msg.sender, reward);
+        userClaimed[msg.sender] += reward;
+        // emit RewardHarvest(msg.sender, reward);
+    }
+
+    function onRewardReceived(uint256 _reward) external {
+        IERC20(payableToken).safeTransferFrom(msg.sender, address(this), _reward);
+        rewardPerTokenNumerator += _reward * REWARD_PER_TOKEN_DENOMINATOR / totalDeposit;
+        // emit RewardShared(msg.sender, reward);
+    }
+    
+    /// @notice Базой для расчёта начислений нужно считать, что мы закладываем фиксированный годовой процент
+    ///   эмиссии токена для релееров, обозначим его как Emission rate
+    ///   Обозначим суммарный стейк релеера в его пуле Relayer pool как Pool Stake=SUM Stakei i=0,..,n,
+    ///   где n - количество записей в контракте Relayer pool.
+    ///   Тогда дневная прибыль валидатора day profit составляет Day profit=Pool Stake*Emission rate/100/365
+    ///   Период начисления наград - один раз сутки.
+    uint256 internal lastHarvestRewardTimestamp;
+
+    function getLastHarvestRewardTimestamp() external view returns(uint256) {
+        return lastHarvestRewardTimestamp;
+    }
+
+    function harvestReward() external {
+        // todo discuss
+        // require(block.timestamp - _lastHarvestRewardTimestamp >= 24*3600)
+
+        // Тогда дневная прибыль валидатора day profit составляет Day profit=Pool Stake*Emission rate/100/365
+        uint256 harvestForPeriod = block.timestamp - lastHarvestRewardTimestamp;
+        uint256 profit = this.getTotalDeposit() * harvestForPeriod * emissionRate;
+        lastHarvestRewardTimestamp = block.timestamp;
+
+        IERC20(payableToken).safeTransferFrom(address(vault), address(this), profit);
+
+        // fee
+        uint256 fee = profit * relayerFeeNumerator / RELAYER_FEE_DENOMINATOR;
+        uint256 rewardForPool = profit - fee;
+
+        this.onRewardReceived(rewardForPool);
+
+        // todo emit
+    }
+    
+    function setRelayerStatus(RelayerStatus _status) external onlyRegistry {
+        require(relayerStatus != _status, Errors.SAME_VALUE);
+        relayerStatus = _status;
+        // emit RelayerStatusSet(_status);
+    }
+    
+    function setRelayerFeeNumerator(uint256 _value) external onlyRegistry {
+        require(_value >= RELAYER_FEE_MIN_NUMERATOR, Errors.FEE_IS_TOO_LOW);
+        require(_value <= RELAYER_FEE_DENOMINATOR, Errors.FEE_IS_TOO_HIGH);
+        relayerFeeNumerator = _value;
+        //  emit RelayerFeeSet(_value);
+    }
+    
+}
+
+
+// todo discuss
+/*
+Бизнес-логика начисления наград:
+1. Контракт Relayer pool начисляет токены и берёт их из некого контракта, где они уже созданы
+2. Контракт Reward управляет логикой начисления этих наград
+3. Базой для расчёта начислений нужно считать, что мы закладываем фиксированный годовой процент эмиссии токена для релееров, обозначим его как Emission rate
+Обозначим суммарный стейк релеера в его пуле Relayer pool как Pool Stake=SUM Stakei i=0,..,n, где n - количество записей в контракте Relayer pool
+Тогда дневная прибыль валидатора day profit составляет Day profit=Pool Stake*Emission rate/100/365
+Период начисления наград - один раз сутки
+4. Обозначим личный стейк (с адреса Relayer owner address) релеера как Owner stake, минимальное его значение равняется COLLATERAL, в случае его вывода из Relayer pool нода релеера переводится контрактом Reward в состояние inactivate путем вызова метода Update relayer status. При вызове
+5. Максимальный суммарный стейк в одиночном пуле Relayer pool составляет личный стейк Owner stake*6
+6. Минимальный размер депозита стейкера в Relayer pool составляет COLLATERAL/1000
+*/
