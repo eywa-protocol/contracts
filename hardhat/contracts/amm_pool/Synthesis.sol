64f298fb8caf704073cd7f671c13be6d3a1029b2
Vadim Gukasov <wadguk1990@gmail.com>
2021-09-12 14:40:06 +0300

    CreateRepresentation method updated

--- hardhat/contracts/amm_pool/Synthesis.sol
+++ hardhat/contracts/amm_pool/Synthesis.sol
@@ -2,35 +2,43 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-newone/token/ERC20/IERC20.sol";
-import '@openzeppelin/contracts-newone/utils/math/SafeMath.sol';
+import "@openzeppelin/contracts-newone/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts-newone/utils/Create2.sol";
 import "./IBridge.sol";
 import "./ISyntERC20.sol";
 import "./SyntERC20.sol";
 import "./RelayRecipient.sol";
 
-
 contract Synthesis is RelayRecipient {
-
-    mapping (address => address) public representationReal;
-    mapping (address => address) public representationSynt;
+    mapping(address => address) public representationReal;
+    mapping(address => address) public representationSynt;
     address[] private keys;
     uint256 requestCount = 1;
-    mapping (bytes32 => TxState) public requests;
-    mapping (bytes32 => SynthesizeState) public synthesizeStates;
+    mapping(bytes32 => TxState) public requests;
+    mapping(bytes32 => SynthesizeState) public synthesizeStates;
     address public bridge;
-    enum RequestState { Default, Sent, Reverted}
-    enum SynthesizeState { Default, Synthesized, RevertRequest}
+    enum RequestState {
+        Default,
+        Sent,
+        Reverted
+    }
+    enum SynthesizeState {
+        Default,
+        Synthesized,
+        RevertRequest
+    }
 
-    event BurnRequest(bytes32 indexed _id, address indexed _from, address indexed _to, uint _amount,address _token);
+    event BurnRequest(bytes32 indexed _id, address indexed _from, address indexed _to, uint256 _amount, address _token);
     event RevertSynthesizeRequest(bytes32 indexed _id, address indexed _to);
-    event SynthesizeCompleted(bytes32 indexed _id, address indexed _to, uint _amount, address _token);
-    event RevertBurnCompleted(bytes32 indexed _id, address indexed _to, uint _amount, address _token);
+    event SynthesizeCompleted(bytes32 indexed _id, address indexed _to, uint256 _amount, address _token);
+    event RevertBurnCompleted(bytes32 indexed _id, address indexed _to, uint256 _amount, address _token);
+    event CreateRepresentation(address indexed _rtoken, address indexed _stoken);
 
-    constructor (address _bridge, address _trustedForwarder) RelayRecipient(_trustedForwarder) {
+    constructor(address _bridge, address _trustedForwarder) RelayRecipient(_trustedForwarder) {
         bridge = _bridge;
     }
 
-    modifier onlyBridge {
+    modifier onlyBridge() {
         require(bridge == msg.sender);
         _;
     }
@@ -45,9 +53,17 @@ contract Synthesis is RelayRecipient {
     }
 
     // can called only by bridge after initiation on a second chain
-    function mintSyntheticToken(bytes32 _txID, address _tokenReal, uint256 _amount, address _to) external onlyBridge {
+    function mintSyntheticToken(
+        bytes32 _txID,
+        address _tokenReal,
+        uint256 _amount,
+        address _to
+    ) external onlyBridge {
         // TODO add chek to Default - чтобы не было по бриджу
-        require(synthesizeStates[_txID] == SynthesizeState.Default, "Synt: emergencyUnsynthesizedRequest called or tokens has been already synthesized");
+        require(
+            synthesizeStates[_txID] == SynthesizeState.Default,
+            "Synt: emergencyUnsynthesizedRequest called or tokens has been already synthesized"
+        );
 
         ISyntERC20(representationSynt[_tokenReal]).mint(_to, _amount);
         synthesizeStates[_txID] = SynthesizeState.Synthesized;
@@ -56,13 +72,17 @@ contract Synthesis is RelayRecipient {
     }
 
     // Revert synthesize() operation, can be called several times
-    function emergencyUnsyntesizeRequest(bytes32 _txID,address _receiveSide, address _oppositeBridge, uint _chainID) external {
-
-        require(synthesizeStates[_txID]!= SynthesizeState.Synthesized, "Synt: syntatic tokens already minted");
-        synthesizeStates[_txID] = SynthesizeState.RevertRequest;// close
-        bytes memory out  = abi.encodeWithSelector(bytes4(keccak256(bytes('emergencyUnsynthesize(bytes32)'))),_txID);
+    function emergencyUnsyntesizeRequest(
+        bytes32 _txID,
+        address _receiveSide,
+        address _oppositeBridge,
+        uint256 _chainID
+    ) external {
+        require(synthesizeStates[_txID] != SynthesizeState.Synthesized, "Synt: syntatic tokens already minted");
+        synthesizeStates[_txID] = SynthesizeState.RevertRequest; // close
+        bytes memory out = abi.encodeWithSelector(bytes4(keccak256(bytes("emergencyUnsynthesize(bytes32)"))), _txID);
         // TODO add payment by token
-        IBridge(bridge).transmitRequestV2(out,_receiveSide, _oppositeBridge, _chainID);
+        IBridge(bridge).transmitRequestV2(out, _receiveSide, _oppositeBridge, _chainID);
 
         emit RevertSynthesizeRequest(_txID, _msgSender());
     }
@@ -74,19 +94,25 @@ contract Synthesis is RelayRecipient {
         address _chain2address,
         address _receiveSide,
         address _oppositeBridge,
-        uint _chainID
+        uint256 _chainID
     ) external returns (bytes32 txID) {
         ISyntERC20(_stoken).burn(_msgSender(), _amount);
         txID = keccak256(abi.encodePacked(this, requestCount));
 
-        bytes memory out  = abi.encodeWithSelector(bytes4(keccak256(bytes('unsynthesize(bytes32,address,uint256,address)'))),txID, representationReal[_stoken], _amount, _chain2address);
+        bytes memory out = abi.encodeWithSelector(
+            bytes4(keccak256(bytes("unsynthesize(bytes32,address,uint256,address)"))),
+            txID,
+            representationReal[_stoken],
+            _amount,
+            _chain2address
+        );
         // TODO add payment by token
         IBridge(bridge).transmitRequestV2(out, _receiveSide, _oppositeBridge, _chainID);
         TxState storage txState = requests[txID];
-        txState.recipient    = _msgSender();
-        txState.chain2address    = _chain2address;
-        txState.stoken     = _stoken;
-        txState.amount     = _amount;
+        txState.recipient = _msgSender();
+        txState.chain2address = _chain2address;
+        txState.stoken = _stoken;
+        txState.amount = _amount;
         txState.state = RequestState.Sent;
 
         requestCount += 1;
@@ -101,7 +127,7 @@ contract Synthesis is RelayRecipient {
         address _chain2address,
         address _receiveSide,
         address _oppositeBridge,
-        uint _chainID
+        uint256 _chainID
     ) external returns (bytes32 txID) {
         (bool _success1, ) = _stoken.call(_approvalData);
         require(_success1, "Approve call failed");
@@ -109,14 +135,20 @@ contract Synthesis is RelayRecipient {
         ISyntERC20(_stoken).burn(_msgSender(), _amount);
         txID = keccak256(abi.encodePacked(this, requestCount));
 
-        bytes memory out  = abi.encodeWithSelector(bytes4(keccak256(bytes('unsynthesize(bytes32,address,uint256,address)'))),txID, representationReal[_stoken], _amount, _chain2address);
+        bytes memory out = abi.encodeWithSelector(
+            bytes4(keccak256(bytes("unsynthesize(bytes32,address,uint256,address)"))),
+            txID,
+            representationReal[_stoken],
+            _amount,
+            _chain2address
+        );
         // TODO add payment by token
         IBridge(bridge).transmitRequestV2(out, _receiveSide, _oppositeBridge, _chainID);
         TxState storage txState = requests[txID];
-        txState.recipient    = _msgSender();
-        txState.chain2address    = _chain2address;
-        txState.stoken     = _stoken;
-        txState.amount     = _amount;
+        txState.recipient = _msgSender();
+        txState.chain2address = _chain2address;
+        txState.stoken = _stoken;
+        txState.amount = _amount;
         txState.state = RequestState.Sent;
 
         requestCount += 1;
@@ -127,18 +159,26 @@ contract Synthesis is RelayRecipient {
     // can called only by bridge after initiation on a second chain
     function emergencyUnburn(bytes32 _txID) external onlyBridge {
         TxState storage txState = requests[_txID];
-        require(txState.state ==  RequestState.Sent, 'Synt: state not open or tx does not exist');
+        require(txState.state == RequestState.Sent, "Synt: state not open or tx does not exist");
         txState.state = RequestState.Reverted; // close
         ISyntERC20(txState.stoken).mint(txState.recipient, txState.amount);
 
         emit RevertBurnCompleted(_txID, txState.recipient, txState.amount, txState.stoken);
     }
 
-    function createRepresentation(address _rtoken, string memory _stokenName,string memory _stokenSymbol) external onlyOwner {
-        //address stoken = representationSynt[_rtoken];
-        //require(stoken == address(0x0), "Synt: token representation already exist");
-        SyntERC20 syntToken = new SyntERC20(_stokenName,_stokenSymbol);
-        setRepresentation(_rtoken, address(syntToken));
+    function createRepresentation(
+        address _rtoken,
+        string memory _stokenName,
+        string memory _stokenSymbol
+    ) external onlyOwner {
+        require(representationSynt[_rtoken] == address(0x0), "Synt: token representation already exist");
+        address stoken = Create2.deploy(
+            0,
+            keccak256(abi.encodePacked(_rtoken)),
+            abi.encodePacked(type(SyntERC20).creationCode, abi.encode(_stokenName, _stokenSymbol))
+        );
+        setRepresentation(_rtoken, stoken);
+        emit CreateRepresentation(_rtoken, stoken);
     }
 
     // should be restricted in mainnets
@@ -151,18 +191,18 @@ contract Synthesis is RelayRecipient {
     }
 
     // utils
-    function setRepresentation (address _rtoken, address _stoken) internal {
+    function setRepresentation(address _rtoken, address _stoken) internal {
         representationSynt[_rtoken] = _stoken;
         representationReal[_stoken] = _rtoken;
         keys.push(_rtoken);
     }
 
-    function getListRepresentation() external view returns(address[] memory, address[] memory) {
-       uint256 len = keys.length;
-       address[] memory sToken = new address[](len);
-       for (uint256 i = 0; i < len; i++) {
-          sToken[i] = representationSynt[keys[i]];
-       }
-       return (keys, sToken);
+    function getListRepresentation() external view returns (address[] memory, address[] memory) {
+        uint256 len = keys.length;
+        address[] memory sToken = new address[](len);
+        for (uint256 i = 0; i < len; i++) {
+            sToken[i] = representationSynt[keys[i]];
+        }
+        return (keys, sToken);
     }
 }
---
+++ hardhat/test/synthesize.test.js
@@ -0,0 +1,62 @@
+const { ethers } = require('hardhat');
+const crypto = require('crypto');
+
+
+function buildCreate2Address(creatorAddress, saltHex, byteCode) {
+    return `0x${web3.utils.sha3(`0x${[
+        'ff',
+        creatorAddress,
+        saltHex,
+        web3.utils.sha3(byteCode)
+    ].map(x => x.replace(/0x/, ''))
+     .join('')}`).slice(-40)}`.toLowerCase()
+}
+
+function getRandomAddress() {
+    return crypto.randomBytes(20).toString('hex')
+}
+
+describe('SYNTHESIS', () => {
+    const TEST_TOKEN_NAME = "TestName"
+    const TEST_TOKEN_SYMBOL = "TestSymbol"
+
+    beforeEach(async () => {
+        accounts = await ethers.getSigners();
+    });
+
+    describe('testing representation...', () => {
+
+        it('Synthesis contract deployed', async function () {
+            Synthesis = await ethers.getContractFactory('Synthesis');
+            synthesis = await Synthesis.deploy(getRandomAddress(), getRandomAddress());
+            ERC20 = await ethers.getContractFactory('SyntERC20');
+            originalToken = await ERC20.deploy(TEST_TOKEN_NAME, TEST_TOKEN_SYMBOL);
+            // expect(await)
+        });
+
+        it('Compute the address and deploy a representation', async function () {
+            const encodedParameters = web3.eth.abi.encodeParameters(
+                ['string', 'string'],
+                [TEST_TOKEN_NAME, TEST_TOKEN_SYMBOL]
+            ).slice(2)
+            const bytecodeWithParam = ERC20.bytecode + encodedParameters;
+            const salt = web3.utils.sha3(web3.utils.toHex(originalToken.address), { encoding: "hex" });
+            const expectedRepresentationAddress = web3.utils.toChecksumAddress(buildCreate2Address(
+                synthesis.address,
+                salt,
+                bytecodeWithParam
+            ))
+
+            tx = await synthesis.createRepresentation(originalToken.address, TEST_TOKEN_NAME, TEST_TOKEN_SYMBOL)
+            receipt = await tx.wait()
+            expect(expectedRepresentationAddress).to.equal(receipt.events[1].args._stoken)
+        });
+
+        it('Should revert with "token representation already exist"', async function () {
+            await expect(synthesis.createRepresentation(originalToken.address, TEST_TOKEN_NAME, TEST_TOKEN_SYMBOL)).to.be.revertedWith('Synt: token representation already exist')
+
+        });
+
+    })
+
+});
