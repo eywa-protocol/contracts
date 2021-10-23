b43dd5576f4a224f27127d9456fa73a4252c3a86
v0ld <v0ld@ai>
2021-10-23 19:11:46 +0700

    avalanche e2e test

--- hardhat/test/integration/e2e.test.js
+++ hardhat/test/integration/e2e.test.js
@@ -1,7 +1,7 @@
 
 let argv = null;
 if(process.env.TYPE_TEST === 'local') argv = {'typenet': 'devstand', 'net1': 'network1','net2': 'network2', 'net3': 'network3' };
-if(process.env.TYPE_TEST === 'testnet') argv = {'typenet': 'teststand', 'net1': 'avalanchetestnet','net2': 'bsctestnet', 'net3': 'rinkeby' };
+if(process.env.TYPE_TEST === 'testnet') argv = {'typenet': 'teststand', 'net1': 'rinkeby','net2': 'bsctestnet', 'net3': 'mumbai', 'net4': 'avalanchetestnet' };
  //require('minimist')(process.argv.slice(3), {string: ['typenet', 'net1', 'net2', 'net3']});
 
 const Web3 = require('web3');
@@ -11,55 +11,59 @@ const {expectRevert} = require('@openzeppelin/test-helpers');
 const mockPool1 = artifacts.require('MockDexPool');
 const mockPool2 = artifacts.require('MockDexPool');
 const mockPool3 = artifacts.require('MockDexPool');
+const mockPool4 = artifacts.require('MockDexPool');
 
 const brigdePart1 = artifacts.require('Bridge');
 const brigdePart2 = artifacts.require('Bridge');
 const brigdePart3 = artifacts.require('Bridge');
+const brigdePart4 = artifacts.require('Bridge');
 
 const factoryProvider = checkoutProvider(argv);
 
 let envNet1 = require('dotenv').config({path: `./networks_env/env_test_for_${argv.net1}.env`});
 let envNet2 = require('dotenv').config({path: `./networks_env/env_test_for_${argv.net2}.env`});
 let envNet3 = require('dotenv').config({path: `./networks_env/env_test_for_${argv.net3}.env`});
+let envNet4 = require('dotenv').config({path: `./networks_env/env_test_for_${argv.net4}.env`});
 
 // todo gas consumtion
 contract('Bridge', (deployer, accounts) => {
 
     before(async () => {
-
+        
         brigdePart1.setProvider(factoryProvider.web3Net1);
         brigdePart2.setProvider(factoryProvider.web3Net2);
         brigdePart3.setProvider(factoryProvider.web3Net3);
-
+        brigdePart4.setProvider(factoryProvider.web3Net4);
+        
         let adr1 = eval(`envNet1.parsed.BRIDGE_${argv.net1.toUpperCase()}`);
         let adr2 = eval(`envNet2.parsed.BRIDGE_${argv.net2.toUpperCase()}`);
         let adr3 = eval(`envNet3.parsed.BRIDGE_${argv.net3.toUpperCase()}`);
-
+        let adr4 = eval(`envNet4.parsed.BRIDGE_${argv.net4.toUpperCase()}`);
+        
         this.br1 = await brigdePart1.at(adr1);
         this.br2 = await brigdePart2.at(adr2);
         this.br3 = await brigdePart3.at(adr3);
-
+        this.br4 = await brigdePart4.at(adr4);
         /** users */
         this.userNet1 = (await brigdePart1.web3.eth.getAccounts())[0];
         this.userNet2 = (await brigdePart2.web3.eth.getAccounts())[0];
         this.userNet3 = (await brigdePart3.web3.eth.getAccounts())[0];
+        this.userNet4 = (await brigdePart4.web3.eth.getAccounts())[0];
 
         /** mock dexpool in one evm based blockchain and in another evm blockchain */
         mockPool1.setProvider(factoryProvider.web3Net1);
         mockPool2.setProvider(factoryProvider.web3Net2);
         mockPool3.setProvider(factoryProvider.web3Net3);
-
-        this.mp1 = null;
-        this.mp2 = null;
-        this.mp3 = null;
+        mockPool4.setProvider(factoryProvider.web3Net4);
 
         this.mp1 = await mockPool1.at(eval(`envNet1.parsed.DEXPOOL_${argv.net1.toUpperCase()}`), {from: this.userNet1});
         this.mp2 = await mockPool2.at(eval(`envNet2.parsed.DEXPOOL_${argv.net2.toUpperCase()}`), {from: this.userNet2});
         this.mp3 = await mockPool3.at(eval(`envNet3.parsed.DEXPOOL_${argv.net3.toUpperCase()}`), {from: this.userNet3});
+        this.mp4 = await mockPool4.at(eval(`envNet4.parsed.DEXPOOL_${argv.net4.toUpperCase()}`), {from: this.userNet4});
     });
 
     describe('simple end-to-end test', async () => {
-        it('From network 1 to 2 without callback', async () => {
+        it.skip('From network 1 to 2 without callback', async () => {
 
             let res = (await this.mp2.testData({from: this.userNet2})).toString();
             let testData = Math.floor((Math.random() * 100) + 1);
@@ -128,6 +132,19 @@ contract('Bridge', (deployer, accounts) => {
                 //console.log(e.message);
                 assert.equal(e.receipt?.status === false ? undefined : e.receipt?.status, undefined, `Should be false by UNTRASTED DEX`);
             }
+        });
+
+        it('From network 4 to 2 without callback', async () => {
+
+            let res = (await this.mp2.testData({from: this.userNet2})).toString();
+            let testData = Math.floor((Math.random() * 100) + 1);
+            /** send end-to-end request */
+            let receipt = await this.mp4.sendRequestTestV2(testData, this.mp2.address, this.br2.address, chainId(argv.net2), {from: this.userNet4, gas: 300_000 });
+            //console.log(receipt);
+            await timeout(25000); // give some time
+            res = (await this.mp2.testData({from: this.userNet2})).toString();
+
+            assert.equal(res, testData, `Should be ${testData}`);
         });
     });
 })
