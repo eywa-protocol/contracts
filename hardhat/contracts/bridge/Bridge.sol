22a501aafc63f833fc1cc724a8d9db2f1269af50
Vadim Gukasov <wadguk1990@gmail.com>
2021-11-10 18:53:22 +0300

    receiveSide to address

--- hardhat/contracts/bridge/Bridge.sol
+++ hardhat/contracts/bridge/Bridge.sol
@@ -2,21 +2,24 @@
 pragma solidity 0.8.0;
 
 import "./core/BridgeCore.sol";
-import "./interface/ListNodeInterface.sol";
+import "./interface/INodeRegistry.sol";
+import "@openzeppelin/contracts-newone/utils/cryptography/ECDSA.sol";
+import "../utils/@opengsn/contracts/src/BaseRelayRecipient.sol";
 
-//TODO: onlyTrustedNode has worse filled data. I.e. In func NodeList#addNode the golang node registers himself
-// and this means every node who wants to start up can add himself in onlyTrustedNode list.
-contract Bridge is BridgeCore {
 
-    constructor (address listNode) {
+contract Bridge is BridgeCore, BaseRelayRecipient  {
+
+  constructor (address listNode, address forwarder) {
         _listNode = listNode;
-        _owner    = msg.sender;
+        trustedForwarder = forwarder;
+
     }
 
     modifier onlyTrustedNode() {
-        require(ListNodeInterface(_listNode).checkPermissionTrustList(msg.sender) == true, "Only trusted node can invoke");
+        require(INodeRegistry(_listNode).checkPermissionTrustList(_msgSender()) == true, "Only trusted node can invoke");
         _;
     }
+
 
     modifier onlyTrustedContract(address receiveSide, address oppositeBridge) {
         require(contractBind[bytes32(uint256(uint160(msg.sender)))][bytes32(uint256(uint160(oppositeBridge)))] == bytes32(uint256(uint160(receiveSide))), "UNTRUSTED CONTRACT");
@@ -46,7 +49,7 @@ contract Bridge is BridgeCore {
         return true;
     }
 
-    function transmitRequestV2(
+    function transmitRequestV2_32(
         bytes memory _selector,
         bytes32 receiveSide,
         bytes32 oppositeBridge,
@@ -68,13 +71,15 @@ contract Bridge is BridgeCore {
     function receiveRequestV2(
         bytes32 reqId,
         bytes memory b,
-        bytes32 receiveSide,
+        address receiveSide,
         bytes32 bridgeFrom
     ) external onlyTrustedNode {
 
-        bytes32 senderSide = contractBind[receiveSide][bridgeFrom];
+        bytes32 senderSide = contractBind[bytes32(uint256(uint160(receiveSide)))][bridgeFrom];
         (bool success, bytes memory data) = address(bytes20(receiveSide)).call(b);
         require(success && (data.length == 0 || abi.decode(data, (bool))), 'FAILED');
         emit ReceiveRequest(reqId, receiveSide, bridgeFrom, senderSide);
     }
+
+     string public override versionRecipient = "2.2.3";
 }
