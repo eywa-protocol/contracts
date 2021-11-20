33f50c1d5e98f91785ca90d3fe22be3f8fb60b98
vaafonchikov <vaafonchikov@fintechiq.ru>
2021-11-20 22:34:54 +0700

    test

--- .scripts/generate.py
+++ .scripts/generate.py
@@ -4,9 +4,14 @@ from os import getenv
 networks = getenv("CI_COMMIT_TAG").split('-')[3]
 action = getenv("CI_COMMIT_TAG").split('-')[2]
 
+print(networks)
+print(action)
+
 j2 = open('.scripts/job_template.j2').read()
 t = Template(j2)
 
 for network in networks:
+    print("generate start")
+    print(network)
     with open('generated-config.yml', 'a') as ci_config:
         ci_config.write(t.render(network=network, action=action))
