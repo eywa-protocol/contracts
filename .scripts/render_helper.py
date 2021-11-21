347e0ff4ba310ef29f69bc7cab51929f0cf29d9e
vaafonchikov <vaafonchikov@fintechiq.ru>
2021-11-21 12:42:55 +0700

    test

--- .scripts/render_helper.py
+++ .scripts/render_helper.py
@@ -11,9 +11,18 @@ with open(new_file) as f:
     new_addresses = f.readlines()
 
 for address in new_addresses:
-    name = address.split('=')[0]
-    addr = address.split('=')[1].strip()
-    data[net_name][name] = addr
+    try:
+        name = address.split('=')[0]
+    except:
+        name = False
+
+    try:
+        addr = address.split('=')[1].strip()
+    except:
+        addr = False
+
+    if addr:
+        data[net_name][name] = addr
 
 with open('/contracts/helper-hardhat-config.json', 'w') as f:
     json.dump(data, f, indent=4)
