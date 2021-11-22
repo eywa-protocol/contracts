5e7335d230215e2f70c07ae5cfb8615ee119d0ff
vaafonchikov <vaafonchikov@fintechiq.ru>
2021-11-22 14:55:44 +0700

    test

--- .scripts/generate.py
+++ .scripts/generate.py
@@ -13,4 +13,9 @@ j2 = open('.scripts/job_template.j2').read()
 t = Template(j2)
 
 with open('generated-config.yml', 'a') as ci_config:
-    ci_config.write(t.render(networks=networks, action=action, CI_COMMIT_TAG=getenv("CI_COMMIT_TAG")))
+    ci_config.write(t.render(networks=networks, action=action,
+                            CI_COMMIT_TAG=getenv("CI_COMMIT_TAG"),
+                            eywa_base_image=getenv("EYWA_BASE_IMAGE"),
+                            eywa_base_tag=getenv("EYWA_BASE_TAG")
+                            )
+                  )
