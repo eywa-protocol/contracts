b9d41c0b7330ad184aee5ef97739d03fabc1d5a4
Denis Glotov <denis@glotov.org>
2021-12-05 12:07:35 +0100

    Attempt to update epochs of all networks in one script run

--- hardhat/scripts/bridge/updateEpoch.js
+++ hardhat/scripts/bridge/updateEpoch.js
@@ -1,25 +1,34 @@
 const fs = require("fs");
 const networkConfig = require('../../helper-hardhat-config.json')
 const hre = require("hardhat");
+const { networks } = require('../../hardhat.config');
+const args = process.env.NETWORKS.split(' ') || [];
 
-const name = hre.network.name;
+function getProvider(url) {
+  return new ethers.providers.JsonRpcProvider(url);
+}
 
 async function main() {
+  return Promise.all(args.map(network => update(network)))
+}
+
+async function update(network) {
+  const provider = getProvider(networks[network].url);
+  const deployer = await provider.getSigner();
   const Bridge = await ethers.getContractFactory("Bridge");
-  const [deployer] = await ethers.getSigners();
-  const bridge  = await Bridge.attach(networkConfig[name].bridge);
-  console.log("Updating epoch. Network:", name, " Owner:", deployer.address, " Bridge:", bridge.address);
+  const bridge  = await Bridge.attach(networkConfig[network].bridge).connect(provider);
+  console.log("Updating epoch. Network:", network, " Owner:", deployer.address, " Bridge:", bridge.address);
 
   const dao = await bridge.dao();
   if (dao === '0x0000000000000000000000000000000000000000') {
     console.log("DAO not set. Setting it to the owner...");
-    await bridge.daoTransferOwnership(deployer.address);
+    await bridge.connect(deployer).daoTransferOwnership(deployer.address);
   } else if (dao !== deployer.address) {
-    console.log("ERROR: DAO is already set to unknown address.");
+    console.log("ERROR: DAO is already set to unknown address", dao);
     return;
   }
 
-  const tx = await bridge.daoUpdateEpochRequest(true, {from: deployer.address});
+  const tx = await bridge.connect(deployer).daoUpdateEpochRequest(true);
   console.log("✓ Epoch update requested at tx", tx.hash);
 }
 
