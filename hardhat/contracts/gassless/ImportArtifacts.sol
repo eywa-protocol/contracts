6cf430456e4984f9075f54766fc821c8406b39ef
Belukov Aleksey <a.beliukov@eywa.fi>
2022-01-13 19:53:19 +0300

    Remove copy of: GsnUtils, IForwarder

--- hardhat/contracts/gassless/ImportArtifacts.sol
+++ hardhat/contracts/gassless/ImportArtifacts.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts-newone/utils/Context.sol";
 import "@openzeppelin/contracts-newone/access/Ownable.sol";
+import "@opengsn/contracts/src/forwarder/IForwarder.sol";
 
 // File: @opengsn/contracts/src/utils/MinLibBytes.sol
 
@@ -101,115 +102,6 @@ library MinLibBytes {
         }
         return result;
     }
-}
-
-// File: @opengsn/contracts/src/utils/GsnUtils.sol
-
-/* solhint-disable no-inline-assembly */
-
-library GsnUtils {
-
-    /**
-     * extract method sig from encoded function call
-     */
-    function getMethodSig(bytes memory msgData) internal pure returns (bytes4) {
-        return MinLibBytes.readBytes4(msgData, 0);
-    }
-
-    /**
-     * extract parameter from encoded-function block.
-     * see: https://solidity.readthedocs.io/en/develop/abi-spec.html#formal-specification-of-the-encoding
-     * the return value should be casted to the right type (uintXXX/bytesXXX/address/bool/enum)
-     */
-    function getParam(bytes memory msgData, uint index) internal pure returns (uint) {
-        return MinLibBytes.readUint256(msgData, 4 + index * 32);
-    }
-
-    //re-throw revert with the same revert data.
-    function revertWithData(bytes memory data) internal pure {
-        assembly {
-            revert(add(data,32), mload(data))
-        }
-    }
-
-}
-
-// File: @opengsn/contracts/src/forwarder/IForwarder.sol
-
-interface IForwarder {
-
-    struct ForwardRequest {
-        address from;
-        address to;
-        uint256 value;
-        uint256 gas;
-        uint256 nonce;
-        bytes data;
-        uint256 validUntil;
-    }
-
-    event DomainRegistered(bytes32 indexed domainSeparator, bytes domainValue);
-
-    event RequestTypeRegistered(bytes32 indexed typeHash, string typeStr);
-
-    function getNonce(address from)
-    external view
-    returns(uint256);
-
-    /**
-     * verify the transaction would execute.
-     * validate the signature and the nonce of the request.
-     * revert if either signature or nonce are incorrect.
-     * also revert if domainSeparator or requestTypeHash are not registered.
-     */
-    function verify(
-        ForwardRequest calldata forwardRequest,
-        bytes32 domainSeparator,
-        bytes32 requestTypeHash,
-        bytes calldata suffixData,
-        bytes calldata signature
-    ) external view;
-
-    /**
-     * execute a transaction
-     * @param forwardRequest - all transaction parameters
-     * @param domainSeparator - domain used when signing this request
-     * @param requestTypeHash - request type used when signing this request.
-     * @param suffixData - the extension data used when signing this request.
-     * @param signature - signature to validate.
-     *
-     * the transaction is verified, and then executed.
-     * the success and ret of "call" are returned.
-     * This method would revert only verification errors. target errors
-     * are reported using the returned "success" and ret string
-     */
-    function execute(
-        ForwardRequest calldata forwardRequest,
-        bytes32 domainSeparator,
-        bytes32 requestTypeHash,
-        bytes calldata suffixData,
-        bytes calldata signature
-    )
-    external payable
-    returns (bool success, bytes memory ret);
-
-    /**
-     * Register a new Request typehash.
-     * @param typeName - the name of the request type.
-     * @param typeSuffix - any extra data after the generic params.
-     *  (must add at least one param. The generic ForwardRequest type is always registered by the constructor)
-     */
-    function registerRequestType(string calldata typeName, string calldata typeSuffix) external;
-
-    /**
-     * Register a new domain separator.
-     * The domain separator must have the following fields: name,version,chainId, verifyingContract.
-     * the chainId is the current network's chainId, and the verifyingContract is this forwarder.
-     * This method is given the domain name and version to create and register the domain separator value.
-     * @param name the domain's display name
-     * @param version the domain/protocol version
-     */
-    function registerDomainSeparator(string calldata name, string calldata version) external;
 }
 
 // File: @opengsn/contracts/src/utils/GsnTypes.sol
