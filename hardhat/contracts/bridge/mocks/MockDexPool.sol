d7ea3c5977178753e8e88562b59955ed556c588b
Denis Glotov <denis@glotov.org>
2022-01-14 10:54:43 +0100

    use source and destination chain ids to calculate request id

--- hardhat/contracts/bridge/mocks/MockDexPool.sol
+++ hardhat/contracts/bridge/mocks/MockDexPool.sol
@@ -39,13 +39,18 @@ contract MockDexPool is SolanaSerialize {
         // ...
 
         uint256 nonce = Bridge(bridge).getNonce(msg.sender);
-        bytes32 requestId = Bridge(bridge).prepareRqId(bytes32(uint256(uint160(oppBridge))),
-                                                       chainId,
-                                                       bytes32(uint256(uint160(secondPartPool))),
-                                                       bytes32(uint256(uint160(msg.sender))),
-                                                       nonce);
-        bytes memory out  = abi.encodeWithSelector(bytes4(keccak256(bytes('receiveRequestTest(uint256,bytes32)'))), testData_, requestId);
-        bool success = Bridge(bridge).transmitRequestV2(out, secondPartPool, oppBridge, chainId, requestId, msg.sender, nonce);
+        bytes32 requestId = Bridge(bridge).prepareRqId(
+            bytes32(uint256(uint160(oppBridge))),
+            chainId,
+            bytes32(uint256(uint160(secondPartPool))),
+            bytes32(uint256(uint160(msg.sender))),
+            nonce);
+        bytes memory output = abi.encodeWithSelector(
+            bytes4(keccak256(bytes('receiveRequestTest(uint256,bytes32)'))),
+            testData_,
+            requestId);
+        bool success = Bridge(bridge).transmitRequestV2(
+            output, secondPartPool, oppBridge, chainId, requestId, msg.sender, nonce);
 
         emit RequestSent(requestId, success);
     }
