1417663f065f4625402b1cce3987d7409f4eba0b
v0ld <v0ld@ai>
2022-01-21 16:41:55 +0700

    fix:
    1. scripts/meta_exchange/deploy-crosschain-pool.js the network names for testnet
    2. scripts/meta_exchange/deploy-hub-pool.js the paramets for testnet
    3. helper-hardhat-config.json.example to etalon structure

--- hardhat/scripts/meta_exchange/deploy-crosschain-pool.js
+++ hardhat/scripts/meta_exchange/deploy-crosschain-pool.js
@@ -55,7 +55,8 @@ async function main() {
   let crosschainPool
 
   // creating local stable tokens for specified networks
-  if (network.name != "network2" && network.name != "mumbai") {
+  //if (network.name != "network2" && network.name != "mumbai") {
+  if (network.name == "rinkeby" || network.name == "bsctestnet") {    
     //empty the array
     deployInfo[network.name].localToken = []
 
@@ -66,12 +67,12 @@ async function main() {
       deployInfo[network.name].localToken.push({ address: localToken[i].address, name: await localToken[i].name(), symbol: await localToken[i].symbol() });
       if (network.name == "network1" || network.name == "network3")
         crosschainPoolCoins.push(await getRepresentation(deployInfo[network.name].localToken[i], deployInfo["network2"].synthesis))
-      if (network.name == "rinkeby" || network.name == "rinkeby" || network.name == "rinkeby" || network.name == "rinkeby" || network.name == "rinkeby")
+      if (network.name == "rinkeby" || network.name == "bsctestnet" || network.name == "rinkeby" || network.name == "rinkeby" || network.name == "rinkeby")
         crosschainPoolCoins.push(await getRepresentation(deployInfo[network.name].localToken[i], deployInfo["mumbai"].synthesis))
     }
     if (network.name == "network1" || network.name == "network3")
       deployInfo["network2"].crosschainPool.push({ network: network.name, address: "", coins: crosschainPoolCoins, lp: [] });
-    if (network.name == "rinkeby" || network.name == "rinkeby" || network.name == "rinkeby" || network.name == "rinkeby" || network.name == "rinkeby")
+    if (network.name == "rinkeby" || network.name == "bsctestnet" || network.name == "rinkeby" || network.name == "rinkeby" || network.name == "rinkeby")
       deployInfo["mumbai"].crosschainPool.push({ network: network.name, address: "", coins: crosschainPoolCoins, lp: [] });
 
   }
