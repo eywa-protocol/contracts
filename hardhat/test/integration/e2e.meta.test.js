538502626398e9a9b51d22fd51883b86f3454a73
k.piliuk <k.piliuk@eywa.fi>
2022-01-26 00:18:55 +0300

    inconstistency check tests

--- hardhat/test/integration/e2e.meta.test.js
+++ hardhat/test/integration/e2e.meta.test.js
@@ -242,4 +242,337 @@ describe("E2E-META local test", () => {
         await timeout(25000)
         assert(this.balanceC2 < await this.tokenC2.balanceOf(this.userNet1))
     })
+    it("Inconsistency test: expected_min_mint_amount", async function () {
+        selectorMetaExchange = web3.eth.abi.encodeFunctionSignature(
+            'transit_synth_batch_meta_exchange_eth((address,address,address,uint256,int128,int128,uint256,int128,uint256,address,address,address,address,address,uint256),address[3],uint256[3],bytes32[3])'
+        )
+
+        CurveProxy.setProvider(factoryProvider.web3Net3)
+        ERC20.setProvider(factoryProvider.web3Net3)
+        Portal.setProvider(factoryProvider.web3Net3)
+
+        CurveProxyB.setProvider(factoryProvider.web3Net1)
+        ERC20B.setProvider(factoryProvider.web3Net1)
+
+        StableSwap2Pool.setProvider(factoryProvider.web3Net2)
+
+        this.userNet3 = (await CurveProxy.web3.eth.getAccounts())[0];
+        this.userNet1 = (await CurveProxyB.web3.eth.getAccounts())[0];
+
+        this.curveProxyA = CurveProxy.at(deployInfo["network3"].curveProxy)
+        this.tokenA1 = await ERC20.at(deployInfo["network3"].localToken[0].address)
+        this.tokenA2 = await ERC20.at(deployInfo["network3"].localToken[1].address)
+        this.tokenA3 = await ERC20.at(deployInfo["network3"].localToken[2].address)
+        this.portal = await Portal.at(deployInfo["network3"].portal)
+        this.tokenC1 = await ERC20B.at(deployInfo["network1"].localToken[0].address)
+        this.tokenC2 = await ERC20B.at(deployInfo["network1"].localToken[1].address)
+        this.tokenC3 = await ERC20B.at(deployInfo["network1"].localToken[2].address)
+
+        this.hubPoolB = StableSwap2Pool.at(deployInfo["network2"].hubPool.address)
+
+        this.balanceC2 = (await this.tokenC2.balanceOf(this.userNet1)).toString()
+        //synthesize params
+        const synthParams = {
+            chain2address: deployInfo["network2"].curveProxy,
+            receiveSide: deployInfo["network2"].curveProxy,
+            oppositeBridge: deployInfo["network2"].bridge,
+            chainID: deployInfo["network2"].chainId
+        }
+
+        //exchange params
+        const i = 1; //Index value for the coin to send
+        const j = 0; //Index value of the coin to receive (lp)
+
+        //withdraw one coin params
+        const x = 1; //Index value of the coin to receive
+
+        let chain2 = new ethers.Wallet(process.env.PRIVATE_KEY_NETWORK1)
+
+        const metaExchangeParams = {
+            add: deployInfo["network2"].crosschainPool[1].address,
+            exchange: deployInfo["network2"].hubPool.address,  //exchange pool address
+            remove: deployInfo["network2"].crosschainPool[0].address,         //remove pool address
+            //add liquidity params
+            expected_min_mint_amount: BigNumber.from("90000000000000000000000000000000000000000000"),
+            //exchange params
+            i: i,                                             //index value for the coin to send
+            j: j,                                             //index value of the coin to receive
+            expected_min_dy: 0,
+            //withdraw one coin params
+            x: x,                                             // index value of the coin to withdraw
+            expected_min_amount: 0,
+            //mint synth params
+            to: chain2.address,
+            //unsynth params (empty in this case)
+            unsynth_token: ZERO_ADDRESS,
+            chain2address: deployInfo["network1"].portal,
+            receiveSide: deployInfo["network1"].portal,
+            oppositeBridge: deployInfo["network1"].bridge,
+            chainID: deployInfo["network1"].chainId
+        }
+
+        const encodedTransitData = web3.eth.abi.encodeParameters(
+            ['address', 'address', 'address', 'uint256', 'int128', 'int128', 'uint256', 'int128', 'uint256',
+                'address', 'address', 'address', 'address', 'address', 'uint256'],
+            [metaExchangeParams.add,
+            metaExchangeParams.exchange,
+            metaExchangeParams.remove,
+            /////
+            metaExchangeParams.expected_min_mint_amount,
+            /////
+            metaExchangeParams.i,
+            metaExchangeParams.j,
+            metaExchangeParams.expected_min_dy,
+            /////
+            metaExchangeParams.x,
+            metaExchangeParams.expected_min_amount,
+            /////
+            metaExchangeParams.to,
+            /////
+            metaExchangeParams.unsynth_token,
+            metaExchangeParams.chain2address,
+            metaExchangeParams.receiveSide,
+            metaExchangeParams.oppositeBridge,
+            metaExchangeParams.chainID
+            ]
+        )
+
+        await this.tokenA1.approve(this.portal.address, totalSupply, { from: this.userNet3, gas: 300_000 })
+        const amounts = new Array(3).fill(ethers.utils.parseEther("0.0"))
+        let testAmount = Math.floor((Math.random() * 100) + 1);
+        amounts[0] = ethers.utils.parseEther(testAmount.toString() + ".0")
+        let tokensToSynth = [this.tokenA1.address, this.tokenA2.address, this.tokenA3.address]
+        await this.portal.synthesize_batch_transit(
+            tokensToSynth,
+            amounts,
+            synthParams,
+            selectorMetaExchange,
+            encodedTransitData,
+            { from: this.userNet3, gas: 300_000 }
+        )
+        await timeout(25000)
+        assert(this.balanceC2 == await this.tokenC2.balanceOf(this.userNet1))
+    })
+    it("Inconsistency test: expected_min_dy", async function () {
+        selectorMetaExchange = web3.eth.abi.encodeFunctionSignature(
+            'transit_synth_batch_meta_exchange_eth((address,address,address,uint256,int128,int128,uint256,int128,uint256,address,address,address,address,address,uint256),address[3],uint256[3],bytes32[3])'
+        )
+
+        CurveProxy.setProvider(factoryProvider.web3Net3)
+        ERC20.setProvider(factoryProvider.web3Net3)
+        Portal.setProvider(factoryProvider.web3Net3)
+
+        CurveProxyB.setProvider(factoryProvider.web3Net1)
+        ERC20B.setProvider(factoryProvider.web3Net1)
+
+        StableSwap2Pool.setProvider(factoryProvider.web3Net2)
+
+        this.userNet3 = (await CurveProxy.web3.eth.getAccounts())[0];
+        this.userNet1 = (await CurveProxyB.web3.eth.getAccounts())[0];
+
+        this.curveProxyA = CurveProxy.at(deployInfo["network3"].curveProxy)
+        this.tokenA1 = await ERC20.at(deployInfo["network3"].localToken[0].address)
+        this.tokenA2 = await ERC20.at(deployInfo["network3"].localToken[1].address)
+        this.tokenA3 = await ERC20.at(deployInfo["network3"].localToken[2].address)
+        this.portal = await Portal.at(deployInfo["network3"].portal)
+        this.tokenC1 = await ERC20B.at(deployInfo["network1"].localToken[0].address)
+        this.tokenC2 = await ERC20B.at(deployInfo["network1"].localToken[1].address)
+        this.tokenC3 = await ERC20B.at(deployInfo["network1"].localToken[2].address)
+
+        this.hubPoolB = StableSwap2Pool.at(deployInfo["network2"].hubPool.address)
+
+        this.balanceC2 = (await this.tokenC2.balanceOf(this.userNet1)).toString()
+        //synthesize params
+        const synthParams = {
+            chain2address: deployInfo["network2"].curveProxy,
+            receiveSide: deployInfo["network2"].curveProxy,
+            oppositeBridge: deployInfo["network2"].bridge,
+            chainID: deployInfo["network2"].chainId
+        }
+
+        //exchange params
+        const i = 1; //Index value for the coin to send
+        const j = 0; //Index value of the coin to receive (lp)
+
+        //withdraw one coin params
+        const x = 1; //Index value of the coin to receive
+
+        let chain2 = new ethers.Wallet(process.env.PRIVATE_KEY_NETWORK1)
+
+        const metaExchangeParams = {
+            add: deployInfo["network2"].crosschainPool[1].address,
+            exchange: deployInfo["network2"].hubPool.address,  //exchange pool address
+            remove: deployInfo["network2"].crosschainPool[0].address,         //remove pool address
+            //add liquidity params
+            expected_min_mint_amount: 0,
+            //exchange params
+            i: i,                                             //index value for the coin to send
+            j: j,                                             //index value of the coin to receive
+            expected_min_dy: BigNumber.from("90000000000000000000000000000000000000000000"),
+            //withdraw one coin params
+            x: x,                                             // index value of the coin to withdraw
+            expected_min_amount: 0,
+            //mint synth params
+            to: chain2.address,
+            //unsynth params (empty in this case)
+            unsynth_token: ZERO_ADDRESS,
+            chain2address: deployInfo["network1"].portal,
+            receiveSide: deployInfo["network1"].portal,
+            oppositeBridge: deployInfo["network1"].bridge,
+            chainID: deployInfo["network1"].chainId
+        }
+
+        const encodedTransitData = web3.eth.abi.encodeParameters(
+            ['address', 'address', 'address', 'uint256', 'int128', 'int128', 'uint256', 'int128', 'uint256',
+                'address', 'address', 'address', 'address', 'address', 'uint256'],
+            [metaExchangeParams.add,
+            metaExchangeParams.exchange,
+            metaExchangeParams.remove,
+            /////
+            metaExchangeParams.expected_min_mint_amount,
+            /////
+            metaExchangeParams.i,
+            metaExchangeParams.j,
+            metaExchangeParams.expected_min_dy,
+            /////
+            metaExchangeParams.x,
+            metaExchangeParams.expected_min_amount,
+            /////
+            metaExchangeParams.to,
+            /////
+            metaExchangeParams.unsynth_token,
+            metaExchangeParams.chain2address,
+            metaExchangeParams.receiveSide,
+            metaExchangeParams.oppositeBridge,
+            metaExchangeParams.chainID
+            ]
+        )
+
+        await this.tokenA1.approve(this.portal.address, totalSupply, { from: this.userNet3, gas: 300_000 })
+        const amounts = new Array(3).fill(ethers.utils.parseEther("0.0"))
+        let testAmount = Math.floor((Math.random() * 100) + 1);
+        amounts[0] = ethers.utils.parseEther(testAmount.toString() + ".0")
+        let tokensToSynth = [this.tokenA1.address, this.tokenA2.address, this.tokenA3.address]
+        await this.portal.synthesize_batch_transit(
+            tokensToSynth,
+            amounts,
+            synthParams,
+            selectorMetaExchange,
+            encodedTransitData,
+            { from: this.userNet3, gas: 300_000 }
+        )
+        await timeout(25000)
+        assert(this.balanceC2 == await this.tokenC2.balanceOf(this.userNet1))
+    })
+    it("Inconsistency test: expected_min_amount", async function () {
+        selectorMetaExchange = web3.eth.abi.encodeFunctionSignature(
+            'transit_synth_batch_meta_exchange_eth((address,address,address,uint256,int128,int128,uint256,int128,uint256,address,address,address,address,address,uint256),address[3],uint256[3],bytes32[3])'
+        )
+
+        CurveProxy.setProvider(factoryProvider.web3Net3)
+        ERC20.setProvider(factoryProvider.web3Net3)
+        Portal.setProvider(factoryProvider.web3Net3)
+
+        CurveProxyB.setProvider(factoryProvider.web3Net1)
+        ERC20B.setProvider(factoryProvider.web3Net1)
+
+        StableSwap2Pool.setProvider(factoryProvider.web3Net2)
+
+        this.userNet3 = (await CurveProxy.web3.eth.getAccounts())[0];
+        this.userNet1 = (await CurveProxyB.web3.eth.getAccounts())[0];
+
+        this.curveProxyA = CurveProxy.at(deployInfo["network3"].curveProxy)
+        this.tokenA1 = await ERC20.at(deployInfo["network3"].localToken[0].address)
+        this.tokenA2 = await ERC20.at(deployInfo["network3"].localToken[1].address)
+        this.tokenA3 = await ERC20.at(deployInfo["network3"].localToken[2].address)
+        this.portal = await Portal.at(deployInfo["network3"].portal)
+        this.tokenC1 = await ERC20B.at(deployInfo["network1"].localToken[0].address)
+        this.tokenC2 = await ERC20B.at(deployInfo["network1"].localToken[1].address)
+        this.tokenC3 = await ERC20B.at(deployInfo["network1"].localToken[2].address)
+
+        this.hubPoolB = StableSwap2Pool.at(deployInfo["network2"].hubPool.address)
+
+        this.balanceC2 = (await this.tokenC2.balanceOf(this.userNet1)).toString()
+        //synthesize params
+        const synthParams = {
+            chain2address: deployInfo["network2"].curveProxy,
+            receiveSide: deployInfo["network2"].curveProxy,
+            oppositeBridge: deployInfo["network2"].bridge,
+            chainID: deployInfo["network2"].chainId
+        }
+
+        //exchange params
+        const i = 1; //Index value for the coin to send
+        const j = 0; //Index value of the coin to receive (lp)
+
+        //withdraw one coin params
+        const x = 1; //Index value of the coin to receive
+
+        let chain2 = new ethers.Wallet(process.env.PRIVATE_KEY_NETWORK1)
+
+        const metaExchangeParams = {
+            add: deployInfo["network2"].crosschainPool[1].address,
+            exchange: deployInfo["network2"].hubPool.address,  //exchange pool address
+            remove: deployInfo["network2"].crosschainPool[0].address,         //remove pool address
+            //add liquidity params
+            expected_min_mint_amount: 0,
+            //exchange params
+            i: i,                                             //index value for the coin to send
+            j: j,                                             //index value of the coin to receive
+            expected_min_dy: 0,
+            //withdraw one coin params
+            x: x,                                             // index value of the coin to withdraw
+            expected_min_amount: BigNumber.from("90000000000000000000000000000000000000000000"),
+            //mint synth params
+            to: chain2.address,
+            //unsynth params (empty in this case)
+            unsynth_token: ZERO_ADDRESS,
+            chain2address: deployInfo["network1"].portal,
+            receiveSide: deployInfo["network1"].portal,
+            oppositeBridge: deployInfo["network1"].bridge,
+            chainID: deployInfo["network1"].chainId
+        }
+
+        const encodedTransitData = web3.eth.abi.encodeParameters(
+            ['address', 'address', 'address', 'uint256', 'int128', 'int128', 'uint256', 'int128', 'uint256',
+                'address', 'address', 'address', 'address', 'address', 'uint256'],
+            [metaExchangeParams.add,
+            metaExchangeParams.exchange,
+            metaExchangeParams.remove,
+            /////
+            metaExchangeParams.expected_min_mint_amount,
+            /////
+            metaExchangeParams.i,
+            metaExchangeParams.j,
+            metaExchangeParams.expected_min_dy,
+            /////
+            metaExchangeParams.x,
+            metaExchangeParams.expected_min_amount,
+            /////
+            metaExchangeParams.to,
+            /////
+            metaExchangeParams.unsynth_token,
+            metaExchangeParams.chain2address,
+            metaExchangeParams.receiveSide,
+            metaExchangeParams.oppositeBridge,
+            metaExchangeParams.chainID
+            ]
+        )
+
+        await this.tokenA1.approve(this.portal.address, totalSupply, { from: this.userNet3, gas: 300_000 })
+        const amounts = new Array(3).fill(ethers.utils.parseEther("0.0"))
+        let testAmount = Math.floor((Math.random() * 100) + 1);
+        amounts[0] = ethers.utils.parseEther(testAmount.toString() + ".0")
+        let tokensToSynth = [this.tokenA1.address, this.tokenA2.address, this.tokenA3.address]
+        await this.portal.synthesize_batch_transit(
+            tokensToSynth,
+            amounts,
+            synthParams,
+            selectorMetaExchange,
+            encodedTransitData,
+            { from: this.userNet3, gas: 300_000 }
+        )
+        await timeout(25000)
+        assert(this.balanceC2 == await this.tokenC2.balanceOf(this.userNet1))
+    })
 })
