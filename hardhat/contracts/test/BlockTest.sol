306ba64957c0071d778f8bcf2a1565e418d0d6f7
Denis Glotov <denis@glotov.org>
2022-03-13 14:45:54 +0100

    BlockTest.sol hotfix

--- hardhat/contracts/test/BlockTest.sol
+++ hardhat/contracts/test/BlockTest.sol
@@ -27,7 +27,7 @@ contract BlockTest {
     }
 
     function oracleRequestTestSolana(
-        address bridge,
+        bytes32 bridge,
         bytes32 requestId,
         bytes calldata selector,
         bytes32 oppositeBridge
@@ -36,4 +36,23 @@ contract BlockTest {
             bridge, requestId, selector, oppositeBridge
         ));
     }
+
+    /*function oracleRequestMerkleVerify(
+        uint64 _blockChainId,
+        bytes32 _blockPrevBlockHash,
+        bytes32 _blockEpochBlockHash,
+        bytes32 _blockTransactionsRoot,
+        uint64 _blockSourceHeigh,
+        uint64 _blockHeight,
+        bytes calldata _auditPath,
+        address _txBridge,
+        bytes32 _txRequestId,
+        bytes calldata _txSelector,
+        address _txReceiveSide
+    ) public pure returns (bool) {
+        bytes32 blockHash = sha256(abi.encodePacked(
+            _blockChainId, _blockPrevBlockHash, _blockEpochBlockHash, _blockTransactionsRoot, _blockSourceHeigh, _blockHeight
+        ));
+        return true;
+    }*/
 }
