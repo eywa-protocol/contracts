c4a120fd4e877e8a8cbc53fb11798defe86dae6b
Roman Romanov <rroman4work@gmail.com>
2022-03-21 19:55:35 +0300

    uncomment BLS

--- hardhat/contracts/bridge/Bridge.sol
+++ hardhat/contracts/bridge/Bridge.sol
@@ -9,14 +9,11 @@ import "@openzeppelin/contracts-newone/utils/cryptography/ECDSA.sol";
 import "../amm_pool/RelayRecipient.sol";
 import "../utils/Typecast.sol";
 
-contract Bridge is BridgeCore, RelayRecipient,
-    // BlsSignatureVerification,
-    Typecast
-{
+contract Bridge is BridgeCore, RelayRecipient, BlsSignatureVerification, Typecast {
     using AddressUpgradeable for address;
 
     string public versionRecipient;
-    // E2Point private epochKey; // Aggregated public key of all paricipants of the current epoch
+    E2Point private epochKey; // Aggregated public key of all paricipants of the current epoch
     address public dao; // Address of the DAO
     uint8 public epochParticipantsNum; // Number of participants contributed to the epochKey
     uint32 public epochNum; // Sequential number of the epoch
@@ -57,58 +54,58 @@ contract Bridge is BridgeCore, RelayRecipient,
         _;
     }
 
-    // function getEpoch()
-    //     public
-    //     view
-    //     returns (
-    //         bytes memory,
-    //         uint8,
-    //         uint32
-    //     )
-    // {
-    //     return (abi.encode(epochKey), epochParticipantsNum, epochNum);
-    // }
+    function getEpoch()
+        public
+        view
+        returns (
+            bytes memory,
+            uint8,
+            uint32
+        )
+    {
+        return (abi.encode(epochKey), epochParticipantsNum, epochNum);
+    }
 
-    // /**
-    //  * @dev Updates current epoch.
-    //  * @param _newKey aggregated public key of all new epoch participants
-    //  * @param _votersPubKey aggregated public key of the old epoch participants, who voted for the update
-    //  * @param _votersSignature aggregated signature of the old epoch participants, who voted for the update
-    //  * @param _votersMask bitmask of old epoch participants, who voted, amoung all participants
-    //  * @param _newEpochParticipantsNum number of the participants
-    //  */
-    // function updateEpoch(
-    //     bytes calldata _newKey,
-    //     bytes calldata _votersPubKey,
-    //     bytes calldata _votersSignature,
-    //     uint256 _votersMask,
-    //     uint8 _newEpochParticipantsNum,
-    //     uint32 _newEpochNum
-    // ) external {
-    //     require(epochNum + 1 == _newEpochNum, "Bridge: wrong epoch number");
+    /**
+     * @dev Updates current epoch.
+     * @param _newKey aggregated public key of all new epoch participants
+     * @param _votersPubKey aggregated public key of the old epoch participants, who voted for the update
+     * @param _votersSignature aggregated signature of the old epoch participants, who voted for the update
+     * @param _votersMask bitmask of old epoch participants, who voted, amoung all participants
+     * @param _newEpochParticipantsNum number of the participants
+     */
+    function updateEpoch(
+        bytes calldata _newKey,
+        bytes calldata _votersPubKey,
+        bytes calldata _votersSignature,
+        uint256 _votersMask,
+        uint8 _newEpochParticipantsNum,
+        uint32 _newEpochNum
+    ) external {
+        require(epochNum + 1 == _newEpochNum, "Bridge: wrong epoch number");
 
-    //     E2Point memory newKey = decodeE2Point(_newKey);
-    //     E2Point memory votersPubKey = decodeE2Point(_votersPubKey);
-    //     E1Point memory votersSignature = decodeE1Point(_votersSignature);
+        E2Point memory newKey = decodeE2Point(_newKey);
+        E2Point memory votersPubKey = decodeE2Point(_votersPubKey);
+        E1Point memory votersSignature = decodeE1Point(_votersSignature);
 
-    //     if (epochKey.x[0] != 0 || epochKey.x[1] != 0) {
-    //         require(popcnt(_votersMask) >= (uint256(epochParticipantsNum) * 2) / 3, "Bridge: not enough participants"); // TODO configure
-    //         require(
-    //             epochParticipantsNum == 256 || _votersMask < (1 << epochParticipantsNum),
-    //             "Bridge: bitmask too big"
-    //         );
-    //         bytes memory data = abi.encodePacked(newKey.x, newKey.y, _newEpochParticipantsNum, _newEpochNum);
-    //         require(
-    //             verifyMultisig(epochKey, votersPubKey, data, votersSignature, _votersMask),
-    //             "Bridge: multisig mismatch"
-    //         );
-    //     }
+        if (epochKey.x[0] != 0 || epochKey.x[1] != 0) {
+            require(popcnt(_votersMask) >= (uint256(epochParticipantsNum) * 2) / 3, "Bridge: not enough participants"); // TODO configure
+            require(
+                epochParticipantsNum == 256 || _votersMask < (1 << epochParticipantsNum),
+                "Bridge: bitmask too big"
+            );
+            bytes memory data = abi.encodePacked(newKey.x, newKey.y, _newEpochParticipantsNum, _newEpochNum);
+            require(
+                verifyMultisig(epochKey, votersPubKey, data, votersSignature, _votersMask),
+                "Bridge: multisig mismatch"
+            );
+        }
 
-    //     emit NewEpoch(abi.encode(epochKey), abi.encode(newKey), false, _newEpochNum);
-    //     epochKey = newKey;
-    //     epochParticipantsNum = _newEpochParticipantsNum; // TODO: require minimum
-    //     epochNum = _newEpochNum;
-    // }
+        emit NewEpoch(abi.encode(epochKey), abi.encode(newKey), false, _newEpochNum);
+        epochKey = newKey;
+        epochParticipantsNum = _newEpochParticipantsNum; // TODO: require minimum
+        epochNum = _newEpochNum;
+    }
 
     /**
      * @dev Transmit crosschain request v2.
@@ -184,17 +181,17 @@ contract Bridge is BridgeCore, RelayRecipient,
         bytes calldata _votersSignature,
         uint256 _votersMask
     ) external {
-        // require(epochKey.x[0] != 0 || epochKey.x[1] != 0, "Bridge: epoch not set");
-        // require(popcnt(_votersMask) >= (uint256(epochParticipantsNum) * 2) / 3, "Bridge: not enough participants"); // TODO configure
-        // require(epochParticipantsNum == 256 || _votersMask < (1 << epochParticipantsNum), "Bridge: bitmask too big");
+        require(epochKey.x[0] != 0 || epochKey.x[1] != 0, "Bridge: epoch not set");
+        require(popcnt(_votersMask) >= (uint256(epochParticipantsNum) * 2) / 3, "Bridge: not enough participants"); // TODO configure
+        require(epochParticipantsNum == 256 || _votersMask < (1 << epochParticipantsNum), "Bridge: bitmask too big");
 
-        // E2Point memory votersPubKey = decodeE2Point(_votersPubKey);
-        // E1Point memory votersSignature = decodeE1Point(_votersSignature);
-        // bytes memory sigData = abi.encodePacked(_reqId, _sel, _receiveSide, _bridgeFrom, epochNum);
-        // require(
-        //     verifyMultisig(epochKey, votersPubKey, sigData, votersSignature, _votersMask),
-        //     "Bridge: multisig mismatch"
-        // );
+        E2Point memory votersPubKey = decodeE2Point(_votersPubKey);
+        E1Point memory votersSignature = decodeE1Point(_votersSignature);
+        bytes memory sigData = abi.encodePacked(_reqId, _sel, _receiveSide, _bridgeFrom, epochNum);
+        require(
+            verifyMultisig(epochKey, votersPubKey, sigData, votersSignature, _votersMask),
+            "Bridge: multisig mismatch"
+        );
 
         bytes memory data = _receiveSide.functionCall(_sel, "Bridge: receiveRequestV2: failed");
         require(
@@ -211,15 +208,15 @@ contract Bridge is BridgeCore, RelayRecipient,
      *                   successfully vote for it
      */
     function daoUpdateEpochRequest(bool resetEpoch) public virtual onlyDao {
-        // bytes memory epochKeyBytes = abi.encode(epochKey);
-        // if (resetEpoch) {
-        //     epochNum++;
-        //     E2Point memory zero;
-        //     emit NewEpoch(epochKeyBytes, abi.encode(zero), true, epochNum);
-        //     epochKey = zero;
-        // } else {
-        //     emit NewEpoch(epochKeyBytes, epochKeyBytes, true, epochNum);
-        // }
+        bytes memory epochKeyBytes = abi.encode(epochKey);
+        if (resetEpoch) {
+            epochNum++;
+            E2Point memory zero;
+            emit NewEpoch(epochKeyBytes, abi.encode(zero), true, epochNum);
+            epochKey = zero;
+        } else {
+            emit NewEpoch(epochKeyBytes, epochKeyBytes, true, epochNum);
+        }
     }
 
     /**
@@ -231,31 +228,31 @@ contract Bridge is BridgeCore, RelayRecipient,
         dao = newDao;
     }
 
-    // function decodeE2Point(bytes memory _pubKey) private pure returns (E2Point memory pubKey) {
-    //     uint256[] memory output = new uint256[](4);
-    //     for (uint256 i = 32; i <= output.length * 32; i += 32) {
-    //         assembly {
-    //             mstore(add(output, i), mload(add(_pubKey, i)))
-    //         }
-    //     }
+    function decodeE2Point(bytes memory _pubKey) private pure returns (E2Point memory pubKey) {
+        uint256[] memory output = new uint256[](4);
+        for (uint256 i = 32; i <= output.length * 32; i += 32) {
+            assembly {
+                mstore(add(output, i), mload(add(_pubKey, i)))
+            }
+        }
 
-    //     pubKey.x[0] = output[0];
-    //     pubKey.x[1] = output[1];
-    //     pubKey.y[0] = output[2];
-    //     pubKey.y[1] = output[3];
-    // }
+        pubKey.x[0] = output[0];
+        pubKey.x[1] = output[1];
+        pubKey.y[0] = output[2];
+        pubKey.y[1] = output[3];
+    }
 
-    // function decodeE1Point(bytes memory _sig) private pure returns (E1Point memory signature) {
-    //     uint256[] memory output = new uint256[](2);
-    //     for (uint256 i = 32; i <= output.length * 32; i += 32) {
-    //         assembly {
-    //             mstore(add(output, i), mload(add(_sig, i)))
-    //         }
-    //     }
+    function decodeE1Point(bytes memory _sig) private pure returns (E1Point memory signature) {
+        uint256[] memory output = new uint256[](2);
+        for (uint256 i = 32; i <= output.length * 32; i += 32) {
+            assembly {
+                mstore(add(output, i), mload(add(_sig, i)))
+            }
+        }
 
-    //     signature.x = output[0];
-    //     signature.y = output[1];
-    // }
+        signature.x = output[0];
+        signature.y = output[1];
+    }
 
     function popcnt(uint256 mask) private pure returns (uint256 cnt) {
         while (mask != 0) {
