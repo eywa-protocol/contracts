c00194e09754e6abd307b6ed100cc24316658743
Denis Glotov <denis@glotov.org>
2022-03-25 19:24:17 +0100

    UpdateEpoch() uses block merkle tree tx

--- hardhat/contracts/bridge/Bridge.sol
+++ hardhat/contracts/bridge/Bridge.sol
@@ -6,19 +6,19 @@ import "@openzeppelin/contracts-newone/utils/cryptography/ECDSA.sol";
 
 import "../amm_pool/RelayRecipient.sol";
 import "../utils/Block.sol";
+import "../utils/Bls.sol";
 import "../utils/Merkle.sol";
 import "../utils/ReqIdFilter.sol";
 import "../utils/Typecast.sol";
-import "./bls/BlsSignatureVerification.sol";
 import "./core/BridgeCore.sol";
 import "./interface/INodeRegistry.sol";
 
-contract Bridge is BridgeCore, RelayRecipient, BlsSignatureVerification, Typecast {
+contract Bridge is BridgeCore, RelayRecipient, Typecast {
     using AddressUpgradeable for address;
     using ReqIdFilter for ReqIdFilter.Data;
 
     string public versionRecipient;
-    E2Point private epochKey; // Aggregated public key of all paricipants of the current epoch
+    Bls.E2Point private epochKey; // Aggregated public key of all paricipants of the current epoch
     address public dao; // Address of the DAO
     uint8 public epochParticipantsNum; // Number of participants contributed to the epochKey
     uint32 public epochNum; // Sequential number of the epoch
@@ -79,44 +79,33 @@ contract Bridge is BridgeCore, RelayRecipient, BlsSignatureVerification, Typecas
 
     /**
      * @dev Updates current epoch.
-     * @param _newKey aggregated public key of all new epoch participants
+     * @param _blockHeader block header serialization
+     * @param _txMerkleProve EpochEvent transaction payload and its Merkle audit path
      * @param _votersPubKey aggregated public key of the old epoch participants, who voted for the update
      * @param _votersSignature aggregated signature of the old epoch participants, who voted for the update
      * @param _votersMask bitmask of old epoch participants, who voted, amoung all participants
-     * @param _newEpochParticipantsNum number of the participants
      */
     function updateEpoch(
-        bytes calldata _newKey,
+        bytes calldata _blockHeader,
+        bytes calldata _txMerkleProve,
         bytes calldata _votersPubKey,
         bytes calldata _votersSignature,
-        uint256 _votersMask,
-        uint8 _newEpochParticipantsNum,
-        uint32 _newEpochNum
+        uint256 _votersMask
     ) external {
-        require(epochNum + 1 == _newEpochNum, "Bridge: wrong epoch number");
-
-        E2Point memory newKey = decodeE2Point(_newKey);
-        E2Point memory votersPubKey = decodeE2Point(_votersPubKey);
-        E1Point memory votersSignature = decodeE1Point(_votersSignature);
-
         if (epochKey.x[0] != 0 || epochKey.x[1] != 0) {
-            require(popcnt(_votersMask) >= (uint256(epochParticipantsNum) * 2) / 3, "Bridge: not enough participants"); // TODO configure
-            require(
-                epochParticipantsNum == 255 || _votersMask < (1 << epochParticipantsNum),
-                "Bridge: bitmask too big"
-            );
-            bytes memory data = abi.encodePacked(newKey.x, newKey.y, _newEpochParticipantsNum, _newEpochNum);
-            require(
-                verifyMultisig(epochKey, votersPubKey, data, votersSignature, _votersMask),
-                "Bridge: multisig mismatch"
-            );
+            Block.verify(_blockHeader, _votersPubKey, _votersSignature, _votersMask, epochKey, epochParticipantsNum);
         }
 
-        emit NewEpoch(abi.encode(epochKey), abi.encode(newKey), false, _newEpochNum);
+        bytes memory payload = Merkle.prove(_txMerkleProve, Block.transactionsRoot(_blockHeader));
+        (uint32 txNewEpochNum, bytes memory txNewKey, uint8 txNewEpochParticipantsNum) = Block.epochRequestTx(payload);
+        
+        require(epochNum + 1 == txNewEpochNum, "wrong epoch number");
+        Bls.E2Point memory newKey = Bls.decodeE2Point(txNewKey);
         epochKey = newKey;
-        epochParticipantsNum = _newEpochParticipantsNum; // TODO: require minimum
-        epochNum = _newEpochNum;
+        epochParticipantsNum = txNewEpochParticipantsNum; // TODO: require minimum
+        epochNum = txNewEpochNum;
         reqIdFilter.clear();
+        emit NewEpoch(abi.encode(epochKey), abi.encode(newKey), false, txNewEpochNum);
     }
 
     /**
@@ -189,17 +178,10 @@ contract Bridge is BridgeCore, RelayRecipient, BlsSignatureVerification, Typecas
         bytes calldata _votersSignature,
         uint256 _votersMask
     ) external {
-        require(epochKey.x[0] != 0 || epochKey.x[1] != 0, "Bridge: epoch not set");
-        require(popcnt(_votersMask) >= (uint256(epochParticipantsNum) * 2) / 3, "Bridge: not enough participants"); // TODO configure
-        require(epochParticipantsNum == 255 || _votersMask < (1 << epochParticipantsNum), "Bridge: bitmask too big");
+        require(epochKey.x[0] != 0 || epochKey.x[1] != 0, "epoch not set");
 
         // Verify the block signature
-        E2Point memory votersPubKey = decodeE2Point(_votersPubKey);
-        E1Point memory votersSignature = decodeE1Point(_votersSignature);
-        require(
-            verifyMultisig(epochKey, votersPubKey, _blockHeader, votersSignature, _votersMask),
-            "Bridge: multisig mismatch"
-        );
+        Block.verify(_blockHeader, _votersPubKey, _votersSignature, _votersMask, epochKey, epochParticipantsNum);
 
         // Verify that the transaction is really in the block
         bytes memory payload = Merkle.prove(_txMerkleProve, Block.transactionsRoot(_blockHeader));
@@ -214,7 +196,7 @@ contract Bridge is BridgeCore, RelayRecipient, BlsSignatureVerification, Typecas
         );
         emit ReceiveRequest(reqId, receiveSide, bytes32(bytes20(bridgeFrom)));
     }
-
+    
     /**
      * @dev Request updating epoch. Only DAO may call it.
      * @param resetEpoch true to reset the epoch to zero so anyone can set up a new one, without any check,
@@ -225,7 +207,7 @@ contract Bridge is BridgeCore, RelayRecipient, BlsSignatureVerification, Typecas
         bytes memory epochKeyBytes = abi.encode(epochKey);
         if (resetEpoch) {
             epochNum++;
-            E2Point memory zero;
+            Bls.E2Point memory zero;
             emit NewEpoch(epochKeyBytes, abi.encode(zero), true, epochNum);
             epochKey = zero;
         } else {
@@ -240,39 +222,6 @@ contract Bridge is BridgeCore, RelayRecipient, BlsSignatureVerification, Typecas
         require(dao == address(0) || _msgSender() == dao, "Bridge: only DAO");
         emit OwnershipTransferred(dao, newDao);
         dao = newDao;
-    }
-
-    function decodeE2Point(bytes memory _pubKey) private pure returns (E2Point memory pubKey) {
-        uint256[] memory output = new uint256[](4);
-        for (uint256 i = 32; i <= output.length * 32; i += 32) {
-            assembly {
-                mstore(add(output, i), mload(add(_pubKey, i)))
-            }
-        }
-
-        pubKey.x[0] = output[0];
-        pubKey.x[1] = output[1];
-        pubKey.y[0] = output[2];
-        pubKey.y[1] = output[3];
-    }
-
-    function decodeE1Point(bytes memory _sig) private pure returns (E1Point memory signature) {
-        uint256[] memory output = new uint256[](2);
-        for (uint256 i = 32; i <= output.length * 32; i += 32) {
-            assembly {
-                mstore(add(output, i), mload(add(_sig, i)))
-            }
-        }
-
-        signature.x = output[0];
-        signature.y = output[1];
-    }
-
-    function popcnt(uint256 mask) private pure returns (uint256 cnt) {
-        while (mask != 0) {
-            mask = mask & (mask - 1);
-            cnt++;
-        }
     }
 
     function setTrustedForwarder(address _forwarder) external onlyOwner {
--- hardhat/contracts/utils/Block.sol
+++ hardhat/contracts/utils/Block.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import "../utils/ZeroCopySource.sol";
+import "../utils/Bls.sol";
 import "../utils/Utils.sol";
+import "../utils/ZeroCopySource.sol";
 
 library Block {
     function transactionsRoot(bytes calldata _payload) internal pure returns (bytes32 txRootHash) {
@@ -15,5 +16,37 @@ library Block {
         (reqId, off) = ZeroCopySource.NextHash(_payload, off);
         (sel, off) = ZeroCopySource.NextVarBytes(_payload, off);
         (receiveSide, off) = ZeroCopySource.NextAddress(_payload, off);
+    }
+
+    function epochRequestTx(bytes memory _payload) internal pure returns (uint32 txNewEpochNum, bytes memory txNewKey, uint8 txNewEpochParticipantsNum) {
+        uint256 off = 0;
+        (txNewEpochNum, off) = ZeroCopySource.NextUint32(_payload, off);
+        (txNewKey, off) = ZeroCopySource.NextVarBytes(_payload, off);
+        (txNewEpochParticipantsNum, off) = ZeroCopySource.NextUint8(_payload, off + 32);
+    }
+
+    function verify(
+        bytes calldata _blockHeader,
+        bytes calldata _votersPubKey,
+        bytes calldata _votersSignature,
+        uint256 _votersMask,
+        Bls.E2Point memory _epochKey,
+        uint8 _epochParticipantsNum
+    ) internal view {
+        Bls.E2Point memory votersPubKey = Bls.decodeE2Point(_votersPubKey);
+        Bls.E1Point memory votersSignature = Bls.decodeE1Point(_votersSignature);
+        require(popcnt(_votersMask) >= (uint256(_epochParticipantsNum) * 2) / 3, "not enough participants");
+        require(_epochParticipantsNum == 255 || _votersMask < (1 << _epochParticipantsNum), "bitmask too big");
+        require(
+            Bls.verifyMultisig(_epochKey, votersPubKey, _blockHeader, votersSignature, _votersMask),
+            "multisig mismatch"
+        );
+    }
+
+    function popcnt(uint256 mask) internal pure returns (uint256 cnt) {
+        while (mask != 0) {
+            mask = mask & (mask - 1);
+            cnt++;
+        }
     }
 }
