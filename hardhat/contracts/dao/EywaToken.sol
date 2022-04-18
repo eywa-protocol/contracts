c09375e4f12d9a3a766d29ed497cbc91a3f2e892
chrollo <chrollowillbefine@chrollowillbefines-MacBook-Air.local>
2022-04-18 17:31:15 +0300

    ChainID fix

--- hardhat/contracts/dao/EywaToken.sol
+++ hardhat/contracts/dao/EywaToken.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.10;
 
-import "@openzeppelin/contracts-newone/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "../utils/draft-ERC20Permit-Harmony.sol";
 import "@openzeppelin/contracts-newone/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract EywaToken is ERC20Permit, ERC20Burnable {
@@ -10,7 +10,7 @@ contract EywaToken is ERC20Permit, ERC20Burnable {
     string public _symbol = "EYWA";
     uint256 public constant _totalSupply = 1_000_000_000;
 
-    constructor(address initialOwner) ERC20Permit(_name) ERC20(_name, _symbol) {
+    constructor(address initialOwner, uint256 harmonyChainID) ERC20Permit(_name, harmonyChainID) ERC20(_name, _symbol) {
         _mint(initialOwner, _totalSupply);
     }
 }
