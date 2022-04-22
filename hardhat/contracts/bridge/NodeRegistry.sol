485545e1d063d9c6ae7ed072b2b834eacc6082eb
wadguk <v.gukasov@eywa.fi>
2022-04-22 15:50:31 +0300

    add set utility token

--- hardhat/contracts/bridge/NodeRegistry.sol
+++ hardhat/contracts/bridge/NodeRegistry.sol
@@ -1,24 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import {EnumerableSet} from '@openzeppelin/contracts-newone/utils/structs/EnumerableSet.sol';
-import {SafeERC20Upgradeable, IERC20Upgradeable} from '@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol';
-import {ERC20Permit, IERC20Permit} from "@openzeppelin/contracts-newone/token/ERC20/extensions/draft-ERC20Permit.sol";
-import {IERC20} from '@openzeppelin/contracts-newone/token/ERC20/IERC20.sol';
+import { EnumerableSet } from "@openzeppelin/contracts-newone/utils/structs/EnumerableSet.sol";
+import { SafeERC20Upgradeable, IERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import { ERC20Permit, IERC20Permit } from "@openzeppelin/contracts-newone/token/ERC20/extensions/draft-ERC20Permit.sol";
+import { IERC20 } from "@openzeppelin/contracts-newone/token/ERC20/IERC20.sol";
 import "./Bridge.sol";
 import "./RelayerPoolFactory.sol";
-
 
 contract NodeRegistry is Bridge {
     using EnumerableSet for EnumerableSet.AddressSet;
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
     struct Node {
-        address owner;     // address of node signer key
-        address pool;      // RelayerPool for this node
-        string  hostId;    // libp2p host ID
-        bytes   blsPubKey; // BLS public key
-        uint256 nodeId;    // absolute sequential number
+        address owner; // address of node signer key
+        address pool; // RelayerPool for this node
+        string hostId; // libp2p host ID
+        bytes blsPubKey; // BLS public key
+        uint256 nodeId; // absolute sequential number
     }
 
     struct Snapshot {
@@ -27,7 +26,7 @@ contract NodeRegistry is Bridge {
         string[] hostIds;
     }
 
-    uint256 constant SnapshotMaxSize = 50;  // maximum epoch participants num
+    uint256 constant SnapshotMaxSize = 50; // maximum epoch participants num
     uint256 public constant MIN_COLLATERAL = 1 ether; // TODO discuss
 
     address public EYWA;
@@ -35,21 +34,11 @@ contract NodeRegistry is Bridge {
     mapping(address => Node) public ownedNodes;
     mapping(string => address) public hostIds;
     Snapshot public snapshot;
-    mapping(address => bool) public poaOwnersWhiteList;
 
     event NewSnapshot(uint256 snapNum);
-    event CreatedRelayer(
-        address indexed owner,
-        address relayerPool,
-        string hostId,
-        bytes blsPubKey,
-        uint256 nodeId
-    );
+    event CreatedRelayer(address indexed owner, address relayerPool, string hostId, bytes blsPubKey, uint256 nodeId);
 
-    function initialize2(
-        address _EYWA,
-        address _forwarder
-    ) public initializer {
+    function initialize2(address _EYWA, address _forwarder) public initializer {
         require(_EYWA != address(0), Errors.ZERO_ADDRESS);
 
         EYWA = _EYWA;
@@ -57,18 +46,12 @@ contract NodeRegistry is Bridge {
     }
 
     modifier isNewNode(address _owner) {
-        require(
-            ownedNodes[_owner].owner == address(0),
-            string(abi.encodePacked("node already exists"))
-        );
+        require(ownedNodes[_owner].owner == address(0), string(abi.encodePacked("node already exists")));
         _;
     }
 
     modifier existingNode(address _owner) {
-        require(
-            ownedNodes[_owner].owner != address(0),
-            string(abi.encodePacked("node does not exist"))
-        );
+        require(ownedNodes[_owner].owner != address(0), string(abi.encodePacked("node does not exist")));
         _;
     }
 
@@ -105,23 +88,47 @@ contract NodeRegistry is Bridge {
         return pubKeys;
     }
 
-
     function nodeExists(address _owner) external view returns (bool) {
         return ownedNodes[_owner].owner != address(0);
     }
 
     function checkPermissionTrustList(address _owner) external view returns (bool) {
-       return ownedNodes[_owner].owner == _owner; // (test only)
-    // return ownedNodes[_owner].status == 1;
+        return ownedNodes[_owner].owner == _owner; // (test only)
+        // return ownedNodes[_owner].status == 1;
     }
 
-    function createRelayer(Node memory _node) external {
-        require(_node.owner == _msgSender(), "only node owner can create node");
-        require(poaOwnersWhiteList[_msgSender()], "is not whitelisted");
+    function createRelayer(
+        Node memory _node,
+        uint256 _deadline,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) external {
+        RelayerPool relayerPool = RelayerPoolFactory.create(
+            _node.owner, // node owner
+            address(EYWA), // depositToken
+            address(EYWA), // rewardToken            (test only)
+            100, // relayerFeeNumerator    (test only)
+            4000, // emissionRateNumerator  (test only)
+            _node.owner // vault                  (test only)
+        );
+        uint256 nodeBalance = IERC20(EYWA).balanceOf(_msgSender());
+        require(nodeBalance >= MIN_COLLATERAL, "insufficient funds");
+        IERC20Permit(EYWA).permit(_msgSender(), address(this), nodeBalance, _deadline, _v, _r, _s);
+        IERC20Upgradeable(EYWA).safeTransferFrom(_msgSender(), address(relayerPool), nodeBalance);
+        _node.pool = address(relayerPool);
         addNode(_node);
     }
 
-    function getSnapshot() external view returns (bytes[] memory, string[] memory, uint256) {
+    function getSnapshot()
+        external
+        view
+        returns (
+            bytes[] memory,
+            string[] memory,
+            uint256
+        )
+    {
         return (snapshot.blsPubKeys, snapshot.hostIds, snapshot.snapNum);
     }
 
@@ -140,12 +147,14 @@ contract NodeRegistry is Bridge {
             indexes[i] = i;
         }
 
-        uint256 rand = uint256(blockhash(block.number-1));  // TODO unsafe
+        uint256 rand = uint256(blockhash(block.number - 1)); // TODO unsafe
         uint256 len = nodes.length();
         if (len > SnapshotMaxSize) len = SnapshotMaxSize;
         for (uint256 i = 0; i < len; i++) {
             // https://en.wikipedia.org/wiki/Linear_congruential_generator
-            unchecked { rand = rand*6364136223846793005 + 1442695040888963407; }   // TODO unsafe
+            unchecked {
+                rand = rand * 6364136223846793005 + 1442695040888963407;
+            } // TODO unsafe
             uint256 j = i + (rand % (nodes.length() - i));
             Node storage n = ownedNodes[nodes.at(indexes[j])];
             snapshot.blsPubKeys.push(n.blsPubKey);
@@ -157,11 +166,7 @@ contract NodeRegistry is Bridge {
         emit NewSnapshot(snapshot.snapNum);
     }
 
-    function addPoaOwner(address _nodeOwner) public onlyOwner{
-        poaOwnersWhiteList[_nodeOwner] = true;
-    }
-    
-    function removePoaOwner(address _nodeOwner) public onlyOwner{
-        poaOwnersWhiteList[_nodeOwner] = false;
+    function setUtilityToken(address _token) public onlyOwner {
+        EYWA = _token;
     }
 }
