dc12d777ef02dc8db6d979e56d353ad573cc1462
v0ld <gysek@yandex.ru>
2022-04-23 16:18:59 +0700

    UPG: deploy token only if recently does't exist

--- hardhat/scripts/deployERC20.js
+++ hardhat/scripts/deployERC20.js
@@ -15,11 +15,13 @@ async function main() {
         const Token = await ethers.getContractFactory("TestToken");
         let i = 0;
         for(let tkn of this.listFutureTokens){
-            let token  = await Token.deploy(tkn.name, tkn.symbol);
-            await token.deployed();
-            console.log(`Token ${tkn.name} address: ${token.address} network ${network.name}`);
-            networkConfig[network.name].token[i].address = token.address;
-            i++;
+            if(tkn.address === '0x0000000000000000000000000000000000000000'){
+                let token  = await Token.deploy(tkn.name, tkn.symbol);
+                await token.deployed();
+                console.log(`Token ${tkn.name} address: ${token.address} network ${network.name}`);
+                networkConfig[network.name].token[i].address = token.address;
+                i++;
+            }
         }
 
         fs.writeFileSync(process.env.HHC_PASS ? process.env.HHC_PASS : "./helper-hardhat-config.json",
