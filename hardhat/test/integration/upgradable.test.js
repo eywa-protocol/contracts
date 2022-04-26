a8584ead4a7051c4542d89f6cc540e61e31a56ee
k.piliuk <k.piliuk@eywa.fi>
2022-04-26 14:05:09 +0300

    initial

---
+++ hardhat/test/integration/upgradable.test.js
@@ -0,0 +1,59 @@
+let deployInfo = require(process.env.HHC_PASS ? process.env.HHC_PASS : '../../helper-hardhat-config.json');
+const { expect } = require("chai");
+const { upgrades,ethers } = require("hardhat");
+
+contract('Contracts upgrade', () => {
+
+    describe("", () => {
+        it("Portal upgrade", async function () {
+            const _Portal = await ethers.getContractFactory("Portal")
+            const _PortalV2 = await ethers.getContractFactory("PortalV2")
+
+            const portal = await upgrades.deployProxy(_Portal, [deployInfo["network1"].bridge, deployInfo["network1"].forwarder], 
+                { initializer: 'initializeFunc' }
+                );
+            await portal.deployed();
+            console.log("Portal address:", portal.address);
+
+            const portalV2 = await upgrades.upgradeProxy(portal.address, _PortalV2)
+            const testValue2 = await portalV2.testFunc();
+            const testValue = await portalV2.testValue();
+            console.log(testValue.toString())
+            expect(testValue > 0);
+        })
+
+        it("Synthesis upgrade", async function () {
+            const _Synthesis = await ethers.getContractFactory("Synthesis")
+            const _SynthesisV2 = await ethers.getContractFactory("SynthesisV2")
+
+            const synthesis = await upgrades.deployProxy(_Synthesis, [deployInfo["network1"].bridge, deployInfo["network1"].forwarder], 
+                { initializer: 'initializeFunc' }
+                );
+            await synthesis.deployed();
+            console.log("Synthesis address:", synthesis.address);
+
+            const synthesisV2 = await upgrades.upgradeProxy(synthesis.address, _SynthesisV2)
+            const testValue2 = await synthesisV2.testFunc();
+            const testValue = await synthesisV2.testValue();
+            console.log(testValue.toString())
+            expect(testValue > 0);
+        })
+
+        it("Bridge upgrade", async function () {
+            const _Bridge = await ethers.getContractFactory("Bridge")
+            const _BridgeV2 = await ethers.getContractFactory("BridgeV2")
+
+            const bridge = await upgrades.deployProxy(_Bridge, [deployInfo["network1"].forwarder], 
+                { initializer: 'initialize' }
+                );
+            await bridge.deployed();
+            console.log("Bridge address:", bridge.address);
+
+            const bridgeV2 = await upgrades.upgradeProxy(bridge.address, _BridgeV2)
+            const testValue2 = await bridgeV2.testFunc();
+            const testValue = await bridgeV2.testValue();
+            console.log(testValue.toString())
+            expect(testValue > 0);
+        })
+    })
+})
