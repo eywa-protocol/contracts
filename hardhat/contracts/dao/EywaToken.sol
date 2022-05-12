61a5337724a49e00e481f0dcb6a93f2f8c3e58b8
wadguk <v.gukasov@eywa.fi>
2022-05-12 15:28:17 +0300

    update constants

--- hardhat/contracts/dao/EywaToken.sol
+++ hardhat/contracts/dao/EywaToken.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts-newone/token/ERC20/extensions/ERC20Burnable.sol";
 import "../utils/draft-ERC20Permit.sol";
 
 contract EywaToken is ERC20Permit, ERC20Burnable {
-    string private _name = "EYWA-Token";
-    string private _symbol = "EYWA";
+    string private constant _name = "EYWA-Token";
+    string private constant _symbol = "EYWA";
     uint256 private constant _totalSupply = 1_000_000_000 ether;
 
     constructor(address initialOwner, uint256 harmonyChainID) ERC20Permit(_name, harmonyChainID) ERC20(_name, _symbol) {
