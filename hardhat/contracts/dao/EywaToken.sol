43fc16c8797e220fe7b0b4dc904e425bfa4bdff8
wadguk <v.gukasov@eywa.fi>
2022-05-12 15:20:29 +0300

    private inner meta-info

--- hardhat/contracts/dao/EywaToken.sol
+++ hardhat/contracts/dao/EywaToken.sol
@@ -6,9 +6,9 @@ import "@openzeppelin/contracts-newone/token/ERC20/extensions/ERC20Burnable.sol"
 import "../utils/draft-ERC20Permit.sol";
 
 contract EywaToken is ERC20Permit, ERC20Burnable {
-    string public _name = "EYWA-Token";
-    string public _symbol = "EYWA";
-    uint256 public constant _totalSupply = 1_000_000_000 ether;
+    string private _name = "EYWA-Token";
+    string private _symbol = "EYWA";
+    uint256 private constant _totalSupply = 1_000_000_000 ether;
 
     constructor(address initialOwner, uint256 harmonyChainID) ERC20Permit(_name, harmonyChainID) ERC20(_name, _symbol) {
         _mint(initialOwner, _totalSupply);
