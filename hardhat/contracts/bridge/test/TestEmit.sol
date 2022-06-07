c5b5ad2ac245d2e1876b98897d43481b9ad48ad4
Denis Glotov <denis@glotov.org>
2022-06-07 17:37:34 +0200

    TestEmit.sol updated

--- hardhat/contracts/bridge/test/TestEmit.sol
+++ hardhat/contracts/bridge/test/TestEmit.sol
@@ -4,18 +4,21 @@ pragma solidity 0.8.10;
 import "../core/BridgeCore.sol";
 
 contract TestEmit is BridgeCore {
-    function testOracleRequest() public {
-        bytes32 requestId = keccak256("");
+    event TestEvent(bytes32 indexed id, address indexed who, string what, uint256 when);
+
+    function testTestEvent(bytes32 id, string calldata what) public {
+        address who = address(this);
+        emit TestEvent(id, who, what, block.timestamp);
+    }
+
+    function testOracleRequest(bytes32 requestId, bytes memory selector) public {
         address bridge = address(this);
-        bytes memory selector;
         uint256 chain = 0xCAFEBABE;
         emit OracleRequest("setRequest", bridge, requestId, selector, bridge, bridge, chain);
     }
 
-    function testReceiveRequest() public {
-        bytes32 requestId = keccak256("");
+    function testReceiveRequest(bytes32 requestId, bytes32 bridgeFrom) public {
         address bridge = address(this);
-        bytes32 bridgeFrom;
         emit ReceiveRequest(requestId, bridge, bridgeFrom);
     }
 }
