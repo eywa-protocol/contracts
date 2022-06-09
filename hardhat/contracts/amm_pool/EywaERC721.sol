57fbb7b509c54a49a67c5c2c1480e50fd82ee10c
wadguk <v.gukasov@eywa.fi>
2022-06-09 18:22:37 +0300

    add NftVestingProxy & EywaERC721

---
+++ hardhat/contracts/amm_pool/EywaERC721.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+
+contract EywaNft is ERC721PresetMinterPauserAutoId {
+    string private _name = "EYWA-NFT";
+    string private _symbol = "EYWA";
+    string private _baseTokenURI = "https://gateway.pinata.cloud/ipfs/";
+
+    constructor() ERC721PresetMinterPauserAutoId(_name, _symbol, _baseTokenURI) {
+    }
+
+}
---
+++ hardhat/contracts/amm_pool/NftVestingProxy.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@uniswap/lib/contracts/libraries/TransferHelper.sol";
+import "../interfaces/IERC20.sol";
+
+contract NftVestingProxy {
+
+    constructor(address vesting) {
+        _vesting = vesting;
+    }
+
+    struct Allocation {
+        address user;
+        uint256 amount;
+        bool claimed;
+    }
+
+    // NFT_ID -> Allocation
+    mapping(uint256 => Allocation) public _allocation;
+
+    address public _vesting;
+    address public _nft;
+
+    function getAllocation(uint256 id) public returns (Allocation memory) {
+        return _allocation[id];
+    }
+
+    function redeemNft(uint256 id) public {
+        require(IERC721(_nft).ownerOf(id) == msg.sender, "Only nft owner");
+        //TODO: split amount (before cliff and after)
+        IERC20(eywaToken).safeTransfer(msg.sender, _allocation[id].amount);
+    }
+}
