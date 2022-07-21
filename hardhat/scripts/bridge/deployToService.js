5e7a96e698a523c7d42d9d6c83707397d212cf7d
v0ld <gysek@yandex.ru>
2022-07-22 02:58:26 +0700

    fix error #2

--- hardhat/scripts/bridge/deployToService.js
+++ hardhat/scripts/bridge/deployToService.js
@@ -17,17 +17,20 @@ async function main() {
         _TokenPOA = await ethers.getContractFactory("TestTokenPermitHarmony");
         tokenPoa = await _TokenPOA.deploy("EYWA-POA", "POAT", networkConfig[network.name].chainId);
         await tokenPoa.deployed();
+        await tokenPoa.deployTransaction.wait();
         _ERC20Permit = await ethers.getContractFactory("TestTokenPermitHarmony");
         EYWA = await _ERC20Permit.deploy("EYWA-TOKEN", "EYWA", networkConfig[network.name].chainId);
     } else {
         _TokenPOA = await ethers.getContractFactory("TokenPOA");
         tokenPoa = await _TokenPOA.deploy("EYWA-POA", "POAT", networkConfig[network.name].chainId);
         await tokenPoa.deployed();
+        await tokenPoa.deployTransaction.wait();
         _ERC20Permit = await ethers.getContractFactory("EywaToken");
         EYWA = await _ERC20Permit.deploy(deployer.address, networkConfig[network.name].chainId);
     }
 
     await EYWA.deployed();
+    await EYWA.deployTransaction.wait();
     networkConfig[network.name].eywa = EYWA.address;
     networkConfig[network.name].tokenPoa = tokenPoa.address;
     networkConfig[network.name].token[networkConfig[network.name].token.findIndex(x => x.name === 'EYWA-POA')]
@@ -40,6 +43,7 @@ async function main() {
     const _Forwarder = await ethers.getContractFactory("Forwarder");
     const forwarder = await _Forwarder.deploy();
     await forwarder.deployed();
+    await forwarder.deployTransaction.wait();
     networkConfig[network.name].forwarder = forwarder.address;
     console.log("Forwarder address:", forwarder.address);
 
@@ -47,6 +51,7 @@ async function main() {
     const _RelayerPoolFactory = await ethers.getContractFactory("RelayerPoolFactory");
     const relayerPoolFactory = await _RelayerPoolFactory.deploy();
     await relayerPoolFactory.deployed();
+    await relayerPoolFactory.deployTransaction.wait();
     networkConfig[network.name].relayerPoolFactory = relayerPoolFactory.address;
     console.log("RelayerPoolFactory address:", relayerPoolFactory.address);
 
@@ -60,6 +65,7 @@ async function main() {
         { initializer: 'initialize2' },
     );
     await bridge.deployed();
+    await bridge.deployTransaction.wait();
     await relayerPoolFactory.setNodeRegistry(bridge.address);
     networkConfig[network.name].nodeRegistry = bridge.address;
     networkConfig[network.name].bridge = bridge.address;
@@ -69,6 +75,7 @@ async function main() {
     const _MockDexPool = await ethers.getContractFactory("MockDexPool");
     const mockDexPool = await _MockDexPool.deploy(bridge.address);
     await mockDexPool.deployed();
+    await mockDexPool.deployTransaction.wait();
     networkConfig[network.name].mockDexPool = mockDexPool.address;
     console.log(`MockDexPool address: ${mockDexPool.address}`);
 
